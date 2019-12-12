02f51487a2088dfb94da0bee1f24616318cb5b8a
joe-conigliaro <joe.conigliaro@gmail.com>
2019-12-13 05:10:30 +1100

    update from master - 8e1c27d

--- v.c
+++ v.c
@@ -1,6 +1,6 @@
-#define V_COMMIT_HASH "0eeb607"
+#define V_COMMIT_HASH "8e1c27d"
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "bd833de"
+#define V_COMMIT_HASH "0eeb607"
 #endif
 #include <inttypes.h>
 
