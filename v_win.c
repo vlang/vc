7c9d9af41c0cb61d4d63ccbbf21f6ae6453029ef
joe-conigliaro <joe.conigliaro@gmail.com>
2020-01-09 21:15:31 +1100

    update from master - 7882312

--- v_win.c
+++ v_win.c
@@ -1,6 +1,6 @@
-#define V_COMMIT_HASH "760448a"
+#define V_COMMIT_HASH "7882312"
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "8a7f2a2"
+#define V_COMMIT_HASH "760448a"
 #endif
 #include <inttypes.h>
 
