6c10e88ed348c8616a5083db3172bf382a0e13e2
joe-conigliaro <joe.conigliaro@gmail.com>
2020-01-21 03:10:32 +1100

    update from master - 64a9f43

--- v.c
+++ v.c
@@ -1,6 +1,6 @@
-#define V_COMMIT_HASH "84438c0"
+#define V_COMMIT_HASH "64a9f43"
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "9b3ac7e"
+#define V_COMMIT_HASH "84438c0"
 #endif
 #include <inttypes.h>
 
