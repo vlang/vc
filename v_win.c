741de9660117fbca4fd3ab477a24e60f9f8886a9
joe-conigliaro <joe.conigliaro@gmail.com>
2020-02-16 06:20:33 +1100

    [v:master] d499116 - fix ci

--- v_win.c
+++ v_win.c
@@ -1,6 +1,6 @@
-#define V_COMMIT_HASH "87e28e6"
+#define V_COMMIT_HASH "d499116"
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "dc90f4f"
+#define V_COMMIT_HASH "87e28e6"
 #endif
 #include <inttypes.h>
 
