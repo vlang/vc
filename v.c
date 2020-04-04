af4bee812a05cd2bc69f3b9d5f8a3d206418e64b
joe-conigliaro <joe.conigliaro@gmail.com>
2020-04-04 22:01:01 +1100

    [v:master] c0d4503 - gl/glfw: update C definitions

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "d562760"
+#define V_COMMIT_HASH "c0d4503"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "4bade9b"
+#define V_COMMIT_HASH "d562760"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "d562760"
+#define V_CURRENT_COMMIT_HASH "c0d4503"
 #endif
 
 typedef struct array array;
