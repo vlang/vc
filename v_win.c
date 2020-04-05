a5f5f15cb0d5a7a4e10818dd24e1c9bfe716f258
joe-conigliaro <joe.conigliaro@gmail.com>
2020-04-06 02:15:18 +1000

    [v:master] 4e01bf4 - sdf

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "e464c4a"
+#define V_COMMIT_HASH "4e01bf4"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "df45932"
+#define V_COMMIT_HASH "e464c4a"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "e464c4a"
+#define V_CURRENT_COMMIT_HASH "4e01bf4"
 #endif
 
 typedef struct array array;
@@ -22496,9 +22496,9 @@ void v__gen__Gen_or_block(v__gen__Gen* g, string var_name, array_v__ast__Stmt st
 	v__gen__Gen_writeln(g, _STR("if (!%.*s.ok) {", var_name.len, var_name.str));
 	v__gen__Gen_writeln(g, _STR("\tstring err = %.*s.v_error;", var_name.len, var_name.str));
 	v__gen__Gen_writeln(g, _STR("\tint errcode = %.*s.ecode;", var_name.len, var_name.str));
-	multi_return_string_string mr_63759 = v__gen__Gen_type_of_last_statement(g, stmts);
-	string last_type = mr_63759.arg0;
-	string type_of_last_expression = mr_63759.arg1;
+	multi_return_string_string mr_64134 = v__gen__Gen_type_of_last_statement(g, stmts);
+	string last_type = mr_64134.arg0;
+	string type_of_last_expression = mr_64134.arg1;
 	if (string_eq(last_type, tos3("v.ast.ExprStmt")) && string_ne(type_of_last_expression, tos3("void"))) {
 		g->indent++;
 		// FOR IN
