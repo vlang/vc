b907f9d275675d7f35a2946cf00f6a139e6bca7a
joe-conigliaro <joe.conigliaro@gmail.com>
2020-04-05 20:00:19 +1000

    [v:master] 75518e5 - ci: extract separate working_with_an_empty_struct_test.v

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "fde88a2"
+#define V_COMMIT_HASH "75518e5"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "5e76d53"
+#define V_COMMIT_HASH "fde88a2"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "fde88a2"
+#define V_CURRENT_COMMIT_HASH "75518e5"
 #endif
 
 typedef struct array array;
