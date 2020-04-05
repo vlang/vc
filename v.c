553f20b0399cb96162146869ec2449c5dc076d28
joe-conigliaro <joe.conigliaro@gmail.com>
2020-04-05 12:10:20 +1000

    [v:master] 05fbe5a - parser: move comments

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "2a06263"
+#define V_COMMIT_HASH "05fbe5a"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "97db4c0"
+#define V_COMMIT_HASH "2a06263"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "2a06263"
+#define V_CURRENT_COMMIT_HASH "05fbe5a"
 #endif
 
 typedef struct array array;
