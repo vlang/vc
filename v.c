08026e91933a100d08b00a6326434d9aca01e111
joe-conigliaro <joe.conigliaro@gmail.com>
2020-04-18 01:25:20 +1000

    [v:master] ed8855c - tests: fix struct_unknown_field.v

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "4a556f7"
+#define V_COMMIT_HASH "ed8855c"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "af30bf9"
+#define V_COMMIT_HASH "4a556f7"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "4a556f7"
+#define V_CURRENT_COMMIT_HASH "ed8855c"
 #endif
 
 
