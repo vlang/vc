6a5a4ac597a46e197eb9e015f40b849cc8af6b5a
joe-conigliaro <joe.conigliaro@gmail.com>
2020-04-19 05:30:20 +1000

    [v:master] 4fe4709 - docs: fix typo

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "2ab2a58"
+#define V_COMMIT_HASH "4fe4709"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "b0e498b"
+#define V_COMMIT_HASH "2ab2a58"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "2ab2a58"
+#define V_CURRENT_COMMIT_HASH "4fe4709"
 #endif
 
 
