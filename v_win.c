59e5115477c8af79df88f5dfa7191617e7ff1dbc
joe-conigliaro <joe.conigliaro@gmail.com>
2020-04-19 07:45:20 +1000

    [v:master] 3d61420 - tests: remove

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "a18201f"
+#define V_COMMIT_HASH "3d61420"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "4fe4709"
+#define V_COMMIT_HASH "a18201f"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "a18201f"
+#define V_CURRENT_COMMIT_HASH "3d61420"
 #endif
 
 
