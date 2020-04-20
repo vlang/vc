fa73fc2359c77f2b96927aa4a2498126137c79e3
joe-conigliaro <joe.conigliaro@gmail.com>
2020-04-20 16:45:17 +1000

    [v:master] e006da7 - parser: fix a bad comment

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "639dc02"
+#define V_COMMIT_HASH "e006da7"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "c1fc768"
+#define V_COMMIT_HASH "639dc02"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "639dc02"
+#define V_CURRENT_COMMIT_HASH "e006da7"
 #endif
 
 
