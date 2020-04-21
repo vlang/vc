246bf167befaaa560d267b6727b389cd3673464b
joe-conigliaro <joe.conigliaro@gmail.com>
2020-04-21 21:45:20 +1000

    [v:master] bc4a576 - flag: fix flag_test.v

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "a8dc0cc"
+#define V_COMMIT_HASH "bc4a576"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "7c1d6b6"
+#define V_COMMIT_HASH "a8dc0cc"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "a8dc0cc"
+#define V_CURRENT_COMMIT_HASH "bc4a576"
 #endif
 
 
