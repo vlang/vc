2b92bd61957501a86b602ec51b22601f2515b89b
joe-conigliaro <joe.conigliaro@gmail.com>
2020-04-22 15:25:20 +1000

    [v:master] aef756a - docs: high order functions

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "50436a0"
+#define V_COMMIT_HASH "aef756a"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "c35e0e4"
+#define V_COMMIT_HASH "50436a0"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "50436a0"
+#define V_CURRENT_COMMIT_HASH "aef756a"
 #endif
 
 
