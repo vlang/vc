ffb9b527e69696ef6d59de86e2addb7b9742bc3c
joe-conigliaro <joe.conigliaro@gmail.com>
2020-04-24 05:45:21 +1000

    [v:master] ba85b8d - Revert "compiler: support setting pref.output_cross_c with -os cross"

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "6180b84"
+#define V_COMMIT_HASH "ba85b8d"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "ebc4137"
+#define V_COMMIT_HASH "6180b84"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "6180b84"
+#define V_CURRENT_COMMIT_HASH "ba85b8d"
 #endif
 
 
@@ -7701,22 +7701,18 @@ multi_return_v__pref__Preferences_string parse_args(array_string args) {
 			res->backend = v__pref__Backend_x64;
 		}else if (string_eq(arg, tos3("-os"))) {
 			string target_os = os__cmdline__option(current_args, tos3("-os"), tos3(""));
-			i++;
-			Option_v__pref__OS target_os_kind = v__pref__os_from_string(target_os);
-			if (!target_os_kind.ok) {
-				string err = target_os_kind.v_error;
-				int errcode = target_os_kind.ecode;
+			Option_v__pref__OS tmp = v__pref__os_from_string(target_os);
+			if (!tmp.ok) {
+				string err = tmp.v_error;
+				int errcode = tmp.ecode;
 				 // typeof it_expr_type: v.ast.CallExpr
 				// last_type: v.ast.ExprStmt
 				// last_expr_result_type: void
-				if (string_eq(target_os, tos3("cross"))) {
-					res->output_cross_c = true;
-					continue;
-				}
 				println(_STR("unknown operating system target `%.*s`", target_os.len, target_os.str));
 				v_exit(1);
 			};
-			res->os = /*opt*/(*(v__pref__OS*)target_os_kind.data);
+			res->os = /*opt*/(*(v__pref__OS*)tmp.data);
+			i++;
 		}else if (string_eq(arg, tos3("-cflags"))) {
 			res->cflags = os__cmdline__option(current_args, tos3("-cflags"), tos3(""));
 			i++;
@@ -7746,9 +7742,9 @@ multi_return_v__pref__Preferences_string parse_args(array_string args) {
 		}else {
 			bool should_continue = false;
 			// FOR IN array
-			array tmp4 = _const_list_of_flags_with_param;
-			for (int tmp5 = 0; tmp5 < tmp4.len; tmp5++) {
-				string flag_with_param = ((string*)tmp4.data)[tmp5];
+			array tmp3 = _const_list_of_flags_with_param;
+			for (int tmp4 = 0; tmp4 < tmp3.len; tmp4++) {
+				string flag_with_param = ((string*)tmp3.data)[tmp4];
 				if (string_eq(_STR("-%.*s", flag_with_param.len, flag_with_param.str), arg)) {
 					should_continue = true;
 					i++;
