f693e0e0b55a51262b14ffd0afba6c1550f7aa77
joe-conigliaro <joe.conigliaro@gmail.com>
2020-04-23 20:10:20 +1000

    [v:master] bea918b - vup: add missing extension to make it work

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "7e40012"
+#define V_COMMIT_HASH "bea918b"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "fb97c2e"
+#define V_COMMIT_HASH "7e40012"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "7e40012"
+#define V_CURRENT_COMMIT_HASH "bea918b"
 #endif
 
 
