6798ecbf35b1910cc9571176c494da7ce0985e08
joe-conigliaro <joe.conigliaro@gmail.com>
2020-04-24 03:10:20 +1000

    [v:master] 7bcca82 - docs: replace remaining  with

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "4f45675"
+#define V_COMMIT_HASH "7bcca82"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "adb8fb1"
+#define V_COMMIT_HASH "4f45675"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "4f45675"
+#define V_CURRENT_COMMIT_HASH "7bcca82"
 #endif
 
 
