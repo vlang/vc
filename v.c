de6f72a7f79f736bc59350a486c09470b02176c9
joe-conigliaro <joe.conigliaro@gmail.com>
2020-04-26 16:35:20 +1000

    [v:master] b898970 - cmd/tools: use same line imports too

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "3a59f57"
+#define V_COMMIT_HASH "b898970"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "a48080a"
+#define V_COMMIT_HASH "3a59f57"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "3a59f57"
+#define V_CURRENT_COMMIT_HASH "b898970"
 #endif
 
 
