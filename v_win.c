a364d6379350aada9a767658cf17cfc16eb23dbf
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-07 06:00:22 +1000

    [v:master] 4b98289 - tests: skip http tests on musl targets for now

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "e33805b"
+#define V_COMMIT_HASH "4b98289"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "b5a1544"
+#define V_COMMIT_HASH "e33805b"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "e33805b"
+#define V_CURRENT_COMMIT_HASH "4b98289"
 #endif
 
 
