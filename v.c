bda3b1f66230e248914178ff8e1445b693f8f7b5
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-11 00:25:21 +1000

    [v:master] 43ec01b - changelog: fix a typo

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "a73660a"
+#define V_COMMIT_HASH "43ec01b"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "0af939c"
+#define V_COMMIT_HASH "a73660a"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "a73660a"
+#define V_CURRENT_COMMIT_HASH "43ec01b"
 #endif
 
 
