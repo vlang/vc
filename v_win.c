bb91be7143a4702aaf553d797e24f36cad2c2637
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-13 03:30:22 +1000

    [v:master] b5b8fda - gitignore: ignore VS Code workspaces

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "7837abf"
+#define V_COMMIT_HASH "b5b8fda"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "f33de1b"
+#define V_COMMIT_HASH "7837abf"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "7837abf"
+#define V_CURRENT_COMMIT_HASH "b5b8fda"
 #endif
 
 
