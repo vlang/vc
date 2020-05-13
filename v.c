ab0a6c50f280f0e30ac25b085e02d1bf30e522c0
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-14 03:30:21 +1000

    [v:master] b2e5ae9 - examples: fix compilation of the automaton for the game_of_life

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "1ca04e6"
+#define V_COMMIT_HASH "b2e5ae9"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "3270545"
+#define V_COMMIT_HASH "1ca04e6"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "1ca04e6"
+#define V_CURRENT_COMMIT_HASH "b2e5ae9"
 #endif
 
 
