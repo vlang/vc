47834c11ea792377291cdc2780a7173e8871e590
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-13 10:00:22 +1000

    [v:master] 5f7a8df - tools/fast: fix unused var, use Stopwatch, .gitignore

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "af334e3"
+#define V_COMMIT_HASH "5f7a8df"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "3e6f7d3"
+#define V_COMMIT_HASH "af334e3"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "af334e3"
+#define V_CURRENT_COMMIT_HASH "5f7a8df"
 #endif
 
 
