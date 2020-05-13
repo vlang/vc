57153c09d46109a0560f1203b389ae0fc0efee74
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-13 15:20:22 +1000

    [v:master] 60dd7a7 - vpm: fix v update ui

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "3fd852d"
+#define V_COMMIT_HASH "60dd7a7"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "5f7a8df"
+#define V_COMMIT_HASH "3fd852d"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "3fd852d"
+#define V_CURRENT_COMMIT_HASH "60dd7a7"
 #endif
 
 
