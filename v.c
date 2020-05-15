2d5446ff02b677d3b64fa38f0a07c46598b58f40
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-16 01:55:22 +1000

    [v:master] 465f0dd - regex: fix unused variable warnings in regex_test.v

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "8de6da0"
+#define V_COMMIT_HASH "465f0dd"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "d73bedc"
+#define V_COMMIT_HASH "8de6da0"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "8de6da0"
+#define V_CURRENT_COMMIT_HASH "465f0dd"
 #endif
 
 
