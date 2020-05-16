fd5a52a0fb6679df68bde1e44936537df2ca6cca
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-17 04:25:22 +1000

    [v:master] a3a19e8 - repl: implement list and reset commands

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "6855996"
+#define V_COMMIT_HASH "a3a19e8"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "48659f4"
+#define V_COMMIT_HASH "6855996"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "6855996"
+#define V_CURRENT_COMMIT_HASH "a3a19e8"
 #endif
 
 
