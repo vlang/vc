913a38951b127fb5a8430dc8875906b90679254f
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-17 05:20:22 +1000

    [v:master] 3e4d99a - vup: show the current V version after an update

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "62cb48d"
+#define V_COMMIT_HASH "3e4d99a"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "a3a19e8"
+#define V_COMMIT_HASH "62cb48d"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "62cb48d"
+#define V_CURRENT_COMMIT_HASH "3e4d99a"
 #endif
 
 
