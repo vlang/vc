1cc14cddd47063270072211d4c29e821a780dfb9
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-18 00:15:22 +1000

    [v:master] a66eebc - doc: clarify the short struct syntax

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "2de127f"
+#define V_COMMIT_HASH "a66eebc"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "6274007"
+#define V_COMMIT_HASH "2de127f"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "2de127f"
+#define V_CURRENT_COMMIT_HASH "a66eebc"
 #endif
 
 
