cf6ed666718b8c0c9e524286f99ce87859acc124
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-19 00:55:22 +1000

    [v:master] 90270d8 - vfmt: handle fixed size arrays

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "9a237c3"
+#define V_COMMIT_HASH "90270d8"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "857276e"
+#define V_COMMIT_HASH "9a237c3"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "9a237c3"
+#define V_CURRENT_COMMIT_HASH "90270d8"
 #endif
 
 
