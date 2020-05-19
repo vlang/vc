ff26e949800a22135a8071354162ed11b4a4b46d
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-19 21:15:22 +1000

    [v:master] 96a8eaa - repl: handle assigning anonymous function to a variable

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "931c846"
+#define V_COMMIT_HASH "96a8eaa"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "0352584"
+#define V_COMMIT_HASH "931c846"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "931c846"
+#define V_CURRENT_COMMIT_HASH "96a8eaa"
 #endif
 
 
