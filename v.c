b1a912324eb04bd4ed2c4ecebdd68b3512707f0b
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-23 03:05:23 +1000

    [v:master] 1043593 - examples: fix const name warnings due to gx.White, etc...

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "acb58a1"
+#define V_COMMIT_HASH "1043593"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "de09e38"
+#define V_COMMIT_HASH "acb58a1"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "acb58a1"
+#define V_CURRENT_COMMIT_HASH "1043593"
 #endif
 
 
