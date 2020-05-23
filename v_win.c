9b03cc62dfc1c1c76828108caccd4e5087a899aa
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-23 19:10:23 +1000

    [v:master] 43eaec3 - fmt: add keep tests for struct init, fix warnings

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "c690c2f"
+#define V_COMMIT_HASH "43eaec3"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "70f0115"
+#define V_COMMIT_HASH "c690c2f"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "c690c2f"
+#define V_CURRENT_COMMIT_HASH "43eaec3"
 #endif
 
 
