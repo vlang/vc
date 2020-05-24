de534ff10c2b28718b360a39045d2429770c28bd
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-25 04:30:22 +1000

    [v:master] 9315589 - tutorials: switch to

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "a0f8005"
+#define V_COMMIT_HASH "9315589"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "23e8c8e"
+#define V_COMMIT_HASH "a0f8005"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "a0f8005"
+#define V_CURRENT_COMMIT_HASH "9315589"
 #endif
 
 
