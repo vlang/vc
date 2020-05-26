9ee50ae1148ce3556a609a320f91bbcf27d737b1
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-26 22:30:22 +1000

    [v:master] 8d10adf - flag: fix a typo

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "2b27072"
+#define V_COMMIT_HASH "8d10adf"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "145b125"
+#define V_COMMIT_HASH "2b27072"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "2b27072"
+#define V_CURRENT_COMMIT_HASH "8d10adf"
 #endif
 
 
