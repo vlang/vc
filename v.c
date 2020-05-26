1b00e68c46f45b48111f3bde7f8b65ddc50cab70
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-26 20:35:23 +1000

    [v:master] e79adc0 - v symlink: use a .symlink/v.bat launcher to avoid global PATH polution

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "7585483"
+#define V_COMMIT_HASH "e79adc0"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "b4eadb9"
+#define V_COMMIT_HASH "7585483"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "7585483"
+#define V_CURRENT_COMMIT_HASH "e79adc0"
 #endif
 
 
