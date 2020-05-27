fb797a2abfa89e65f94ca70c91a9df82389d13b2
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-27 23:30:23 +1000

    [v:master] e0db880 - fmt: simplify map_init

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "a4af4f9"
+#define V_COMMIT_HASH "e0db880"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "0b64e21"
+#define V_COMMIT_HASH "a4af4f9"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "a4af4f9"
+#define V_CURRENT_COMMIT_HASH "e0db880"
 #endif
 
 
