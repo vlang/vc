8a3975051ba2141b661d01e9f0deb11cc6aa57aa
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-31 19:20:23 +1000

    [v:master] 3a36ed3 - doc: fix broken markdown about

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "bfb926a"
+#define V_COMMIT_HASH "3a36ed3"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "f87e872"
+#define V_COMMIT_HASH "bfb926a"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "bfb926a"
+#define V_CURRENT_COMMIT_HASH "3a36ed3"
 #endif
 
 
