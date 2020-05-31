2091d640696832bb4bd8086b919376143b84e9eb
joe-conigliaro <joe.conigliaro@gmail.com>
2020-05-31 16:05:24 +1000

    [v:master] c4241f9 - docs: fix typo

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "b17e10c"
+#define V_COMMIT_HASH "c4241f9"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "bb9d268"
+#define V_COMMIT_HASH "b17e10c"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "b17e10c"
+#define V_CURRENT_COMMIT_HASH "c4241f9"
 #endif
 
 
