765879f37ec2eec5330c81dd8bced534942789f6
joe-conigliaro <joe.conigliaro@gmail.com>
2020-06-02 17:40:23 +1000

    [v:master] ad8ed85 - examples: make tetris compile

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "138d730"
+#define V_COMMIT_HASH "ad8ed85"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "9fcb853"
+#define V_COMMIT_HASH "138d730"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "138d730"
+#define V_CURRENT_COMMIT_HASH "ad8ed85"
 #endif
 
 
