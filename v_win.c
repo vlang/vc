f42b03ca963d1b3cd4b522bf0ac9ce66189239b3
joe-conigliaro <joe.conigliaro@gmail.com>
2020-06-05 00:25:25 +1000

    [v:master] e2976b7 - readme: minor clean up

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "6b6c9d6"
+#define V_COMMIT_HASH "e2976b7"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "dd7ebf7"
+#define V_COMMIT_HASH "6b6c9d6"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "6b6c9d6"
+#define V_CURRENT_COMMIT_HASH "e2976b7"
 #endif
 
 
