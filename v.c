6ac28301eceb2549304c5e08ad36e4e9eed6113d
joe-conigliaro <joe.conigliaro@gmail.com>
2020-06-05 15:25:24 +1000

    [v:master] 2237968 - gg: fix x11 scaling

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "e7f52eb"
+#define V_COMMIT_HASH "2237968"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "6118875"
+#define V_COMMIT_HASH "e7f52eb"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "e7f52eb"
+#define V_CURRENT_COMMIT_HASH "2237968"
 #endif
 
 
