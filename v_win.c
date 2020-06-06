549c9352d7517820be080f7556a105460fcea621
joe-conigliaro <joe.conigliaro@gmail.com>
2020-06-07 00:55:25 +1000

    [v:master] fb6be08 - fmt: fix compilation

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "7a5f008"
+#define V_COMMIT_HASH "fb6be08"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "2770077"
+#define V_COMMIT_HASH "7a5f008"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "7a5f008"
+#define V_CURRENT_COMMIT_HASH "fb6be08"
 #endif
 
 
