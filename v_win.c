d90888d5c0519ea634dd1555ed3deb9bb49bad66
joe-conigliaro <joe.conigliaro@gmail.com>
2020-06-09 22:50:24 +1000

    [v:master] 67fcce2 - readme: glfw is no longer needed

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "85c7415"
+#define V_COMMIT_HASH "67fcce2"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "b3e416f"
+#define V_COMMIT_HASH "85c7415"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "85c7415"
+#define V_CURRENT_COMMIT_HASH "67fcce2"
 #endif
 
 
