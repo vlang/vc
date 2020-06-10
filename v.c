18845f92f88bc631ca388c6a0c010e3dcc04d399
joe-conigliaro <joe.conigliaro@gmail.com>
2020-06-11 00:20:25 +1000

    [v:master] a43b8b5 - vfmt: add maps_in_fn_args__keep.vv test for 3a369f5

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "3a369f5"
+#define V_COMMIT_HASH "a43b8b5"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "acf0b84"
+#define V_COMMIT_HASH "3a369f5"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "3a369f5"
+#define V_CURRENT_COMMIT_HASH "a43b8b5"
 #endif
 
 
