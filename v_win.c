e78febbf25c6ae82c8179e822b8619e69d0ef11f
joe-conigliaro <joe.conigliaro@gmail.com>
2020-06-10 19:55:24 +1000

    [v:master] b904d7d - vweb: debug prints behind  debug

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "931bba1"
+#define V_COMMIT_HASH "b904d7d"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "c261ac4"
+#define V_COMMIT_HASH "931bba1"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "931bba1"
+#define V_CURRENT_COMMIT_HASH "b904d7d"
 #endif
 
 
