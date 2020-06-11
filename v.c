28be03c53bc15432014d4000a19491b23cc76f99
joe-conigliaro <joe.conigliaro@gmail.com>
2020-06-11 15:00:26 +1000

    [v:master] 6c087de - doc: mention charptr in the primitives section too

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "dc72d25"
+#define V_COMMIT_HASH "6c087de"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "684a443"
+#define V_COMMIT_HASH "dc72d25"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "dc72d25"
+#define V_CURRENT_COMMIT_HASH "6c087de"
 #endif
 
 
