018ea21f4f084607e435371dfdd93282c32f172d
joe-conigliaro <joe.conigliaro@gmail.com>
2020-06-12 23:15:10 +1000

    [v:master] 0838080 - tools: add todo to gen_vc
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "b13c95e"
+#define V_COMMIT_HASH "0838080"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "56ae379"
+#define V_COMMIT_HASH "b13c95e"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "b13c95e"
+#define V_CURRENT_COMMIT_HASH "0838080"
 #endif
 
 
