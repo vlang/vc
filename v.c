e2c2c1ed321b14e26e832ef10443911c5b6aea89
joe-conigliaro <joe.conigliaro@gmail.com>
2020-06-12 16:36:52 +1000

    [v:master] 0845a9b - tools: update gen_vc init method

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "438adfc"
+#define V_COMMIT_HASH "0845a9b"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "47ffedc"
+#define V_COMMIT_HASH "438adfc"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "438adfc"
+#define V_CURRENT_COMMIT_HASH "0845a9b"
 #endif
 
 
