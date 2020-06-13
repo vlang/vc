ed80e9f0aad1b3f3f834ce21a9a994355ecc6e61
joe-conigliaro <joe@vlang.io>
2020-06-14 02:45:24 +1000

    [v:master] 0115c5e - ci: bring back -autofree self test
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "6113c4f"
+#define V_COMMIT_HASH "0115c5e"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "6ce7bb4"
+#define V_COMMIT_HASH "6113c4f"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "6113c4f"
+#define V_CURRENT_COMMIT_HASH "0115c5e"
 #endif
 
 
