2e4a551bd15f46b1a421e757bcf5d736be32cbba
joe-conigliaro <joe@vlang.io>
2020-06-15 21:45:25 +1000

    [v:master] 5af6a68 - vfmt: fix for 'File abc could not be read' bug
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "b0138e0"
+#define V_COMMIT_HASH "5af6a68"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "e73ed56"
+#define V_COMMIT_HASH "b0138e0"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "b0138e0"
+#define V_CURRENT_COMMIT_HASH "5af6a68"
 #endif
 
 
