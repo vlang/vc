f5d47be4d1e529645d168c2fc6f93138090088c1
joe-conigliaro <joe@vlang.io>
2020-06-16 01:30:25 +1000

    [v:master] 62a872b - vdoc: fix  const formatting
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "0cd9066"
+#define V_COMMIT_HASH "62a872b"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "91df872"
+#define V_COMMIT_HASH "0cd9066"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "0cd9066"
+#define V_CURRENT_COMMIT_HASH "62a872b"
 #endif
 
 
