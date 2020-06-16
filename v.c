284fdc825759cc6f0007a8b49bd26ef1fad11912
joe-conigliaro <joe@vlang.io>
2020-06-16 20:30:25 +1000

    [v:master] 651a203 - examples: fix sqlite.v
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "9d6b402"
+#define V_COMMIT_HASH "651a203"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "e8f8def"
+#define V_COMMIT_HASH "9d6b402"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "9d6b402"
+#define V_CURRENT_COMMIT_HASH "651a203"
 #endif
 
 
