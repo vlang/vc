1c2818068ea453b039076ccd8cae04d12fa3ad3b
joe-conigliaro <joe@vlang.io>
2020-06-16 20:25:25 +1000

    [v:master] 9d6b402 - test: fix sqlite_test.v
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "e8f8def"
+#define V_COMMIT_HASH "9d6b402"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "2daf915"
+#define V_COMMIT_HASH "e8f8def"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "e8f8def"
+#define V_CURRENT_COMMIT_HASH "9d6b402"
 #endif
 
 
