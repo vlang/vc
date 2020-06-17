f3b109ea425483deb352f13faf8b56c988a35ae2
joe-conigliaro <joe@vlang.io>
2020-06-18 04:25:24 +1000

    [v:master] 60bbad4 - x64: fix a test
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "a602673"
+#define V_COMMIT_HASH "60bbad4"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "9c9f641"
+#define V_COMMIT_HASH "a602673"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "a602673"
+#define V_CURRENT_COMMIT_HASH "60bbad4"
 #endif
 
 
