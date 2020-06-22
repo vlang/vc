1b96a05b1af5c733d4826def381aedf100694e8f
joe-conigliaro <joe@vlang.io>
2020-06-22 19:10:25 +1000

    [v:master] 640bbba - waitgroup: remove races, make platform independent, document
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "e65879d"
+#define V_COMMIT_HASH "640bbba"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "011e26c"
+#define V_COMMIT_HASH "e65879d"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "e65879d"
+#define V_CURRENT_COMMIT_HASH "640bbba"
 #endif
 
 
