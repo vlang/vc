932cba4eadda2d3b9ca2b0a46efe1929142313a0
joe-conigliaro <joe@vlang.io>
2020-06-23 03:40:25 +1000

    [v:master] 837df7c - test-fixed: enable again vlib/sync/pool_test.v on windows
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "d19a33c"
+#define V_COMMIT_HASH "837df7c"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "a4b159a"
+#define V_COMMIT_HASH "d19a33c"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "d19a33c"
+#define V_CURRENT_COMMIT_HASH "837df7c"
 #endif
 
 
