65615afe49c0a74e6e1a8c573e1a83ccfa404a47
joe-conigliaro <joe@vlang.io>
2020-06-22 23:30:26 +1000

    [v:master] 504fd01 - test-fixed: skip vlib/sync/pool_test.v on windows for now
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "b930a11"
+#define V_COMMIT_HASH "504fd01"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "d232833"
+#define V_COMMIT_HASH "b930a11"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "b930a11"
+#define V_CURRENT_COMMIT_HASH "504fd01"
 #endif
 
 
