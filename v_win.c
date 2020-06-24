4828e46a4d8cbb76b47cb70af4ad8e386381efd8
joe-conigliaro <joe@vlang.io>
2020-06-24 21:45:25 +1000

    [v:master] 7665114 - 0.3 roadmap
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "8652f42"
+#define V_COMMIT_HASH "7665114"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "852fca2"
+#define V_COMMIT_HASH "8652f42"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "8652f42"
+#define V_CURRENT_COMMIT_HASH "7665114"
 #endif
 
 
