7bd40774fb0f87faa37de96dc104cfa95fd99eb9
joe-conigliaro <joe@vlang.io>
2020-06-29 00:00:27 +1000

    [v:master] 9cbf572 - tests: fix a typo
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "9feaff3"
+#define V_COMMIT_HASH "9cbf572"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "bc9f8ba"
+#define V_COMMIT_HASH "9feaff3"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "9feaff3"
+#define V_CURRENT_COMMIT_HASH "9cbf572"
 #endif
 
 
