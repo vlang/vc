6510bcff55294c33e549d8039eaa7c1c481b458d
joe-conigliaro <joe@vlang.io>
2020-06-28 20:40:25 +1000

    [v:master] 0661273 - vfmt: add support for sql x {} expressions and statements
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "b924c6c"
+#define V_COMMIT_HASH "0661273"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "f5fca00"
+#define V_COMMIT_HASH "b924c6c"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "b924c6c"
+#define V_CURRENT_COMMIT_HASH "0661273"
 #endif
 
 
