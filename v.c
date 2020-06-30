cb49b9e2bb968752deeff82436b9cdeb809d2b50
joe-conigliaro <joe@vlang.io>
2020-06-30 22:40:26 +1000

    [v:master] 55989b3 - doc: split array example & some tweaks (#5564)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "d091f4e"
+#define V_COMMIT_HASH "55989b3"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "8a46911"
+#define V_COMMIT_HASH "d091f4e"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "d091f4e"
+#define V_CURRENT_COMMIT_HASH "55989b3"
 #endif
 
 
