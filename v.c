8737b799a22f85073bb495212a6f84e747d6b144
joe-conigliaro <joe@vlang.io>
2020-06-30 22:10:26 +1000

    [v:master] a45ad47 - ci: testing building gitly with -autofree
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "a3086a2"
+#define V_COMMIT_HASH "a45ad47"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "73da3c9"
+#define V_COMMIT_HASH "a3086a2"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "a3086a2"
+#define V_CURRENT_COMMIT_HASH "a45ad47"
 #endif
 
 
