96f8306660ce7889bb9c8e0d0d57abaedc29289e
joe-conigliaro <joe@vlang.io>
2020-07-04 00:20:26 +1000

    [v:master] df2749d - vfmt: fix indent level of commented match branches
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "b7175b5"
+#define V_COMMIT_HASH "df2749d"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "f03688e"
+#define V_COMMIT_HASH "b7175b5"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "b7175b5"
+#define V_CURRENT_COMMIT_HASH "df2749d"
 #endif
 
 
