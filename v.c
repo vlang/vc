2ab7251289bd53182db62c883b8188d7cdb95546
joe-conigliaro <joe@vlang.io>
2020-07-04 23:50:26 +1000

    [v:master] b018381 - vfmt: fix  in
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "fe50bdc"
+#define V_COMMIT_HASH "b018381"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "68af464"
+#define V_COMMIT_HASH "fe50bdc"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "fe50bdc"
+#define V_CURRENT_COMMIT_HASH "b018381"
 #endif
 
 
