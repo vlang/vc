2dc3f8587df5913a74f6ad4da89c7bdf01ab7a65
joe-conigliaro <joe@vlang.io>
2020-07-05 03:00:26 +1000

    [v:master] fbfd92a - tutorials: more blog fixes (#5657)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "0626ac2"
+#define V_COMMIT_HASH "fbfd92a"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "be04de9"
+#define V_COMMIT_HASH "0626ac2"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "0626ac2"
+#define V_CURRENT_COMMIT_HASH "fbfd92a"
 #endif
 
 
