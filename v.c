d5a0c6c327bae56b08ad1e8b6d2c620e0d3cd9b2
joe-conigliaro <joe@vlang.io>
2020-07-05 15:50:26 +1000

    [v:master] 0fb28eb - contributing.md: add link to the Github Handbook's flow section
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,12 +1,12 @@
-#define V_COMMIT_HASH "b525553"
+#define V_COMMIT_HASH "0fb28eb"
 
 #ifndef V_COMMIT_HASH
-#define V_COMMIT_HASH "188bad4"
+#define V_COMMIT_HASH "b525553"
 #endif
 
 
 #ifndef V_CURRENT_COMMIT_HASH
-#define V_CURRENT_COMMIT_HASH "b525553"
+#define V_CURRENT_COMMIT_HASH "0fb28eb"
 #endif
 
 
