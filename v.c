e6055848a01219e9f2dec1b43c50256e760313ac
joe-conigliaro <joe@vlang.io>
2020-07-09 06:20:27 +1000

    [v:master] 373aa3d - vweb: router: fix trailing / (#5760)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "e3581c5"
+#define V_COMMIT_HASH "373aa3d"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "b8d762d"
+	#define V_COMMIT_HASH "e3581c5"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "e3581c5"
+	#define V_CURRENT_COMMIT_HASH "373aa3d"
 #endif
 
 // V typedefs:
