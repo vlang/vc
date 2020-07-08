bbfdc7e202444f725ec3a2d96feafff136d374e8
joe-conigliaro <joe@vlang.io>
2020-07-09 05:50:27 +1000

    [v:master] e3581c5 - gg: disable freetype on ios for now
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "b8d762d"
+#define V_COMMIT_HASH "e3581c5"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "2790890"
+	#define V_COMMIT_HASH "b8d762d"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "b8d762d"
+	#define V_CURRENT_COMMIT_HASH "e3581c5"
 #endif
 
 // V typedefs:
