9ea6ac292cded33cd2a8b239c8491cfa0fdbd15a
joe-conigliaro <joe@vlang.io>
2020-07-10 01:20:26 +1000

    [v:master] 3792e17 - vweb: fix routing for 0 params (#5773)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "c5dc1a3"
+#define V_COMMIT_HASH "3792e17"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "9511b7d"
+	#define V_COMMIT_HASH "c5dc1a3"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "c5dc1a3"
+	#define V_CURRENT_COMMIT_HASH "3792e17"
 #endif
 
 // V typedefs:
