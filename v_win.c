f3d3dbdc16b51cd27ac53241b8b665a4396120c6
joe-conigliaro <joe@vlang.io>
2020-07-10 17:35:26 +1000

    [v:master] beb9043 - ci: add a separate 'Install markdown' step to the gitly-compiles job
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "d3e676b"
+#define V_COMMIT_HASH "beb9043"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "581603f"
+	#define V_COMMIT_HASH "d3e676b"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "d3e676b"
+	#define V_CURRENT_COMMIT_HASH "beb9043"
 #endif
 
 // V typedefs:
