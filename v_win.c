70c7fceeb660069e139f17f02842bf52b89261d5
joe-conigliaro <joe@vlang.io>
2020-07-11 09:45:26 +1000

    [v:master] 006d260 - vfmt: keep , in module  in struct inits too
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "646df49"
+#define V_COMMIT_HASH "006d260"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "7248d84"
+	#define V_COMMIT_HASH "646df49"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "646df49"
+	#define V_CURRENT_COMMIT_HASH "006d260"
 #endif
 
 // V typedefs:
