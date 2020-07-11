6ff34957e3ee50403b94f0f4c167a20607904102
joe-conigliaro <joe@vlang.io>
2020-07-12 09:50:26 +1000

    [v:master] 40da32a - gg: text_width()
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "e747dfd"
+#define V_COMMIT_HASH "40da32a"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "25c397f"
+	#define V_COMMIT_HASH "e747dfd"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "e747dfd"
+	#define V_CURRENT_COMMIT_HASH "40da32a"
 #endif
 
 // V typedefs:
