0136f6dbe8e90beea487e1f3a2486f1056f58ee4
joe-conigliaro <joe@vlang.io>
2020-07-17 05:25:25 +1000

    [v:master] 69ef43b - vweb: fix ip()
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "cb1009d"
+#define V_COMMIT_HASH "69ef43b"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "9b2cbda"
+	#define V_COMMIT_HASH "cb1009d"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "cb1009d"
+	#define V_CURRENT_COMMIT_HASH "69ef43b"
 #endif
 
 // V typedefs:
