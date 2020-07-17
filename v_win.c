37b17e55ca3b0e1e7a4c01789f345440c9b85888
joe-conigliaro <joe@vlang.io>
2020-07-17 10:40:28 +1000

    [v:master] 82e2b1e - strconv: use import instead of module in test files (#5856)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "69ef43b"
+#define V_COMMIT_HASH "82e2b1e"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "cb1009d"
+	#define V_COMMIT_HASH "69ef43b"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "69ef43b"
+	#define V_CURRENT_COMMIT_HASH "82e2b1e"
 #endif
 
 // V typedefs:
