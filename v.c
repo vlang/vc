8aeaad14ba99e8403bc19d28d21e17460d5d48e0
joe-conigliaro <joe@vlang.io>
2020-07-18 23:00:26 +1000

    [v:master] b3011b4 - examples: add example for using the SMTP module (#5870)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "de0fc53"
+#define V_COMMIT_HASH "b3011b4"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "ca83444"
+	#define V_COMMIT_HASH "de0fc53"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "de0fc53"
+	#define V_CURRENT_COMMIT_HASH "b3011b4"
 #endif
 
 // V typedefs:
