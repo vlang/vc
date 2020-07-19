69089d54c82af614d40f828ba6ca4412ad8a64c1
joe-conigliaro <joe@vlang.io>
2020-07-20 02:20:25 +1000

    [v:master] 9c3aa8b - sqlite: add more detailed C.sqlite3_ APIs
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "1114fd2"
+#define V_COMMIT_HASH "9c3aa8b"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "b3011b4"
+	#define V_COMMIT_HASH "1114fd2"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "1114fd2"
+	#define V_CURRENT_COMMIT_HASH "9c3aa8b"
 #endif
 
 // V typedefs:
