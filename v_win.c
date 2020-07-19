63f12d37bb13d1edd7351cc3d9691ca78751b8fc
joe-conigliaro <joe@vlang.io>
2020-07-20 03:10:25 +1000

    [v:master] 0b0f9c9 - readme: simpler bootstrap test #5874
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "9c3aa8b"
+#define V_COMMIT_HASH "0b0f9c9"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "1114fd2"
+	#define V_COMMIT_HASH "9c3aa8b"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "9c3aa8b"
+	#define V_CURRENT_COMMIT_HASH "0b0f9c9"
 #endif
 
 // V typedefs:
