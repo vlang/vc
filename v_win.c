1efc6d9256346c090066d33ca85bb70c786e3fba
joe-conigliaro <joe@vlang.io>
2020-07-23 01:40:25 +1000

    [v:master] b0d76c5 - websocket utf8: move utf8 functions from websocket to encoding.utf8, add utf8_test.v (4/4) (#5924)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "38aa5d6"
+#define V_COMMIT_HASH "b0d76c5"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "0d8ebf5"
+	#define V_COMMIT_HASH "38aa5d6"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "38aa5d6"
+	#define V_CURRENT_COMMIT_HASH "b0d76c5"
 #endif
 
 // V typedefs:
