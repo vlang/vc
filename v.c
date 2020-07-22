715484044aa8b4d40b395c32739309f08024bb7d
joe-conigliaro <joe@vlang.io>
2020-07-23 01:55:26 +1000

    [v:master] ebbc7bd - examples: fix crash when running examples/ws/client.v outside of valgrind
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "b0d76c5"
+#define V_COMMIT_HASH "ebbc7bd"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "38aa5d6"
+	#define V_COMMIT_HASH "b0d76c5"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "b0d76c5"
+	#define V_CURRENT_COMMIT_HASH "ebbc7bd"
 #endif
 
 // V typedefs:
