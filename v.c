3c6893302aaaad660e1d6b3b04a91655f1015e27
joe-conigliaro <joe@vlang.io>
2020-07-23 03:55:26 +1000

    [v:master] 4096432 - cli: prevent need for unsafe block (#5937)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "d820a97"
+#define V_COMMIT_HASH "4096432"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "938e71b"
+	#define V_COMMIT_HASH "d820a97"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "d820a97"
+	#define V_CURRENT_COMMIT_HASH "4096432"
 #endif
 
 // V typedefs:
