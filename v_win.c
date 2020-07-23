361c9e2301130a3958bec1ca712db5279ace3d81
joe-conigliaro <joe@vlang.io>
2020-07-24 06:50:25 +1000

    [v:master] 2eee274 - doc: document C string literals
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "32c1042"
+#define V_COMMIT_HASH "2eee274"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "653d40b"
+	#define V_COMMIT_HASH "32c1042"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "32c1042"
+	#define V_CURRENT_COMMIT_HASH "2eee274"
 #endif
 
 // V typedefs:
