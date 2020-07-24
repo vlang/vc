46e1b447d04c2c296902b8ef7155177b89956302
joe-conigliaro <joe@vlang.io>
2020-07-24 21:00:25 +1000

    [v:master] bba0edf - rand: add rand.uuid_v4()
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "bfca55b"
+#define V_COMMIT_HASH "bba0edf"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "2cf8a53"
+	#define V_COMMIT_HASH "bfca55b"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "bfca55b"
+	#define V_CURRENT_COMMIT_HASH "bba0edf"
 #endif
 
 // V typedefs:
