1968263ecedceb2cf0c9cd06fa37cfd2a9de6fd1
joe-conigliaro <joe@vlang.io>
2020-07-25 21:45:27 +1000

    [v:master] d5b837e - rand: speed up rand.uuid_v4() ~7x, with -prod
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "c1e14b4"
+#define V_COMMIT_HASH "d5b837e"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "3c1427a"
+	#define V_COMMIT_HASH "c1e14b4"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "c1e14b4"
+	#define V_CURRENT_COMMIT_HASH "d5b837e"
 #endif
 
 // V typedefs:
