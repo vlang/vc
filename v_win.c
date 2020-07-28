022d0ffd85eb4e26b8b5ab6c62b6365803763f24
joe-conigliaro <joe@vlang.io>
2020-07-28 18:10:27 +1000

    [v:master] d7ed3cd - fmt: fix SQL formatting in ORM expressions - remove trailing spaces (#5996)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "28657fe"
+#define V_COMMIT_HASH "d7ed3cd"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "8b66816"
+	#define V_COMMIT_HASH "28657fe"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "28657fe"
+	#define V_CURRENT_COMMIT_HASH "d7ed3cd"
 #endif
 
 // V typedefs:
