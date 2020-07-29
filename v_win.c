75414780c97d67c025be1d8c8c4b2b1cd67a796f
joe-conigliaro <joe@vlang.io>
2020-07-30 00:55:27 +1000

    [v:master] 2f2463a - ws: make Client.state  (#6009)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "280afb2"
+#define V_COMMIT_HASH "2f2463a"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "51c9b49"
+	#define V_COMMIT_HASH "280afb2"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "280afb2"
+	#define V_CURRENT_COMMIT_HASH "2f2463a"
 #endif
 
 // V typedefs:
