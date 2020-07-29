b743977fc2e44bafa394ddb733cae4f48fe60d7c
joe-conigliaro <joe@vlang.io>
2020-07-30 01:15:26 +1000

    [v:master] 7b630f0 - ws: send unsolicited pong control frame on connect in ws_test.v (#6010)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "2f2463a"
+#define V_COMMIT_HASH "7b630f0"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "280afb2"
+	#define V_COMMIT_HASH "2f2463a"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "2f2463a"
+	#define V_CURRENT_COMMIT_HASH "7b630f0"
 #endif
 
 // V typedefs:
