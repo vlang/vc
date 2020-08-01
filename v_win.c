360a6d02d3997bf2ca6b91dce260c9d840210703
joe-conigliaro <joe@vlang.io>
2020-08-02 07:55:26 +1000

    [v:master] cc57b52 - gg: fix typo
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "149de95"
+#define V_COMMIT_HASH "cc57b52"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "a1593e7"
+	#define V_COMMIT_HASH "149de95"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "149de95"
+	#define V_CURRENT_COMMIT_HASH "cc57b52"
 #endif
 
 // V typedefs:
