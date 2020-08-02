4728f37d78ac663c5b5a050d4f202cf33032468e
joe-conigliaro <joe@vlang.io>
2020-08-03 01:05:26 +1000

    [v:master] 79b7000 - examples: simplify gg/rectangles.v
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "c6ae322"
+#define V_COMMIT_HASH "79b7000"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "8dcc3cd"
+	#define V_COMMIT_HASH "c6ae322"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "c6ae322"
+	#define V_CURRENT_COMMIT_HASH "79b7000"
 #endif
 
 // V typedefs:
