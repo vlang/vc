a2a1fc98357ebf2da6d77a6cc200af0f06c56494
joe-conigliaro <joe@vlang.io>
2020-08-04 20:00:29 +1000

    [v:master] 216b6bf - gg: remove tracing println in draw_image
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "9ce75e1"
+#define V_COMMIT_HASH "216b6bf"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "f3817c4"
+	#define V_COMMIT_HASH "9ce75e1"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "9ce75e1"
+	#define V_CURRENT_COMMIT_HASH "216b6bf"
 #endif
 
 // V typedefs:
