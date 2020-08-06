28f11555749ece806c4b29647d9194e0ed423f02
joe-conigliaro <joe@vlang.io>
2020-08-06 23:30:28 +1000

    [v:master] 863cf8a - sync: channel implementation (#6074)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "09f1362"
+#define V_COMMIT_HASH "863cf8a"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "b22711e"
+	#define V_COMMIT_HASH "09f1362"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "09f1362"
+	#define V_CURRENT_COMMIT_HASH "863cf8a"
 #endif
 
 // V typedefs:
