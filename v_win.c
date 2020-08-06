2d9a44a0cada2bfbe2852516b07490c1a45c0e2a
joe-conigliaro <joe@vlang.io>
2020-08-07 01:35:26 +1000

    [v:master] d63daa0 - sync: use  on windows-tcc (#6079)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "ea76a33"
+#define V_COMMIT_HASH "d63daa0"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "863cf8a"
+	#define V_COMMIT_HASH "ea76a33"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "ea76a33"
+	#define V_CURRENT_COMMIT_HASH "d63daa0"
 #endif
 
 // V typedefs:
