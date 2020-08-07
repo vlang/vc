02ac0aba4c8adc2c2cf984fd43b7bc6809489842
joe-conigliaro <joe@vlang.io>
2020-08-07 23:50:32 +1000

    [v:master] 55b8cc1 - tests: parallelize compiler_errors_test.v using channels & threads
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "34d0380"
+#define V_COMMIT_HASH "55b8cc1"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "ce4ee2b"
+	#define V_COMMIT_HASH "34d0380"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "34d0380"
+	#define V_CURRENT_COMMIT_HASH "55b8cc1"
 #endif
 
 // V typedefs:
