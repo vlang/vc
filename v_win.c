7c1f8fd3f877a5cb9d0c5ccd720f544409fd66c7
joe-conigliaro <joe@vlang.io>
2020-08-09 12:15:27 +1000

    [v:master] f6a85d5 - net.html: create html parser module (#6076)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "64f218e"
+#define V_COMMIT_HASH "f6a85d5"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "c65dbe5"
+	#define V_COMMIT_HASH "64f218e"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "64f218e"
+	#define V_CURRENT_COMMIT_HASH "f6a85d5"
 #endif
 
 // V typedefs:
