ceff45a0ed4c7433de0ee4957ad9942e6260d590
joe-conigliaro <joe@vlang.io>
2020-08-10 14:25:28 +1000

    [v:master] 20a65cf - sync/channels: implement  method (#6098)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "fce106c"
+#define V_COMMIT_HASH "20a65cf"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "c7fae4d"
+	#define V_COMMIT_HASH "fce106c"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "fce106c"
+	#define V_CURRENT_COMMIT_HASH "20a65cf"
 #endif
 
 // V typedefs:
