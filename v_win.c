f997450e7738aa09fa96b240cdac84f6371f38cb
joe-conigliaro <joe@vlang.io>
2020-08-10 20:15:30 +1000

    [v:master] 5e2824e - vfmt: align field attributes (when no inline comment after type) (#6089)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "bd32f09"
+#define V_COMMIT_HASH "5e2824e"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "20a65cf"
+	#define V_COMMIT_HASH "bd32f09"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "bd32f09"
+	#define V_CURRENT_COMMIT_HASH "5e2824e"
 #endif
 
 // V typedefs:
