0cc892a5878739dffa67470a590c4c92a6c0aa6d
joe-conigliaro <joe@vlang.io>
2020-08-12 01:55:26 +1000

    [v:master] 433610b - sync/channels: provide  and  (#6104)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "9fdb170"
+#define V_COMMIT_HASH "433610b"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "99798b8"
+	#define V_COMMIT_HASH "9fdb170"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "9fdb170"
+	#define V_CURRENT_COMMIT_HASH "433610b"
 #endif
 
 // V typedefs:
