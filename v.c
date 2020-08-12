05ba48f82daab5b4907123220f1f3d40dbf750bf
joe-conigliaro <joe@vlang.io>
2020-08-13 03:15:27 +1000

    [v:master] 9478ff4 - doc: sorting arrays
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "3b5d562"
+#define V_COMMIT_HASH "9478ff4"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "f8be211"
+	#define V_COMMIT_HASH "3b5d562"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "3b5d562"
+	#define V_CURRENT_COMMIT_HASH "9478ff4"
 #endif
 
 // V typedefs:
