1b5e39f61620a9a2553b7425e865e8045f0f0cdb
joe-conigliaro <joe@vlang.io>
2020-08-13 03:00:28 +1000

    [v:master] 3b5d562 - vet: skip tests
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "f8be211"
+#define V_COMMIT_HASH "3b5d562"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "e5e31f7"
+	#define V_COMMIT_HASH "f8be211"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "f8be211"
+	#define V_CURRENT_COMMIT_HASH "3b5d562"
 #endif
 
 // V typedefs:
