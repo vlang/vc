6aacdf0a85f9b25191a95c91cc42c9b4464565ce
joe-conigliaro <joe@vlang.io>
2020-08-20 00:10:28 +1000

    [v:master] eabc2da - v.mod: remove commas; add example for v.vmod (#6169)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "e6e1011"
+#define V_COMMIT_HASH "eabc2da"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "55b0254"
+	#define V_COMMIT_HASH "e6e1011"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "e6e1011"
+	#define V_CURRENT_COMMIT_HASH "eabc2da"
 #endif
 
 // V typedefs:
