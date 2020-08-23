3087680dea191ab8a25acf7a44a25d474ae2fee1
joe-conigliaro <joe@vlang.io>
2020-08-24 00:20:27 +1000

    [v:master] bb60b42 - examples: vfmt calculator.v
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "1c9e020"
+#define V_COMMIT_HASH "bb60b42"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "542b149"
+	#define V_COMMIT_HASH "1c9e020"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "1c9e020"
+	#define V_CURRENT_COMMIT_HASH "bb60b42"
 #endif
 
 // V typedefs:
