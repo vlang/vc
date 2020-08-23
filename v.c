f65e9f3484316557ddb79a5245fc3cb7175da8b6
joe-conigliaro <joe@vlang.io>
2020-08-23 15:50:27 +1000

    [v:master] 542b149 - sokol: fix examples and remove copy pasta
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "94c3a91"
+#define V_COMMIT_HASH "542b149"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "da452c9"
+	#define V_COMMIT_HASH "94c3a91"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "94c3a91"
+	#define V_CURRENT_COMMIT_HASH "542b149"
 #endif
 
 // V typedefs:
