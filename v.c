c4d1375845c4e10943d59721f0328f24ac961cc9
joe-conigliaro <joe@vlang.io>
2020-08-23 15:30:28 +1000

    [v:master] 94c3a91 - gg: update sokol and stb_truetype
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "da452c9"
+#define V_COMMIT_HASH "94c3a91"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "5407d9b"
+	#define V_COMMIT_HASH "da452c9"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "da452c9"
+	#define V_CURRENT_COMMIT_HASH "94c3a91"
 #endif
 
 // V typedefs:
