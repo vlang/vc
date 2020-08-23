7e8f035990abcd9ecb10ce697acbc1a4922ac147
joe-conigliaro <joe@vlang.io>
2020-08-24 04:20:27 +1000

    [v:master] 2e589a5 - sokol.audio: add proper #flag for windows (-lole32)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "e71aff7"
+#define V_COMMIT_HASH "2e589a5"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "dad2c66"
+	#define V_COMMIT_HASH "e71aff7"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "e71aff7"
+	#define V_CURRENT_COMMIT_HASH "2e589a5"
 #endif
 
 // V typedefs:
