27343a24aee528a6f27d407ce7d2bfba38e00607
joe-conigliaro <joe@vlang.io>
2020-08-24 05:20:27 +1000

    [v:master] fa8f5df - sokol: fix compilation of 2048 on some linux distros (undefined XIQueryVersion and XcursorImageCreate)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "2e589a5"
+#define V_COMMIT_HASH "fa8f5df"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "e71aff7"
+	#define V_COMMIT_HASH "2e589a5"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "2e589a5"
+	#define V_CURRENT_COMMIT_HASH "fa8f5df"
 #endif
 
 // V typedefs:
