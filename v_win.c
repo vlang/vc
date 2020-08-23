f63624f94e93f5b838c847cabb75664c6d351ab1
joe-conigliaro <joe@vlang.io>
2020-08-23 22:30:27 +1000

    [v:master] 1c9e020 - sokol.audio: add simple_sin_tones.v example
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "542b149"
+#define V_COMMIT_HASH "1c9e020"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "94c3a91"
+	#define V_COMMIT_HASH "542b149"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "542b149"
+	#define V_CURRENT_COMMIT_HASH "1c9e020"
 #endif
 
 // V typedefs:
