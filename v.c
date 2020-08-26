87cb93aebd3924b7c066ab0f5562e99014ff782a
joe-conigliaro <joe@vlang.io>
2020-08-26 18:50:27 +1000

    [v:master] 99dd72e - tests: add another test for 47f59d3
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "47f59d3"
+#define V_COMMIT_HASH "99dd72e"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "84b8e0a"
+	#define V_COMMIT_HASH "47f59d3"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "47f59d3"
+	#define V_CURRENT_COMMIT_HASH "99dd72e"
 #endif
 
 // V typedefs:
