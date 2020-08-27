df2bdd9ad67084cc180c3e06d0da99c27025972d
joe-conigliaro <joe@vlang.io>
2020-08-27 19:10:27 +1000

    [v:master] 8b39902 - v.checker: run vfmt over vlib/v/checker/checker.v
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "3e9dc57"
+#define V_COMMIT_HASH "8b39902"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "8dcc739"
+	#define V_COMMIT_HASH "3e9dc57"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "3e9dc57"
+	#define V_CURRENT_COMMIT_HASH "8b39902"
 #endif
 
 // V typedefs:
