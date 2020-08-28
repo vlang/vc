6e3464c5fbe176174e3a7ce9cf446d25792f7f64
joe-conigliaro <joe@vlang.io>
2020-08-28 18:25:26 +1000

    [v:master] 789674b - tests: autofree: use -experimental flag
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "0890562"
+#define V_COMMIT_HASH "789674b"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "88f75fc"
+	#define V_COMMIT_HASH "0890562"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "0890562"
+	#define V_CURRENT_COMMIT_HASH "789674b"
 #endif
 
 // V typedefs:
