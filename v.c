ad38332b7efabc763d91182d20a905100152e6a7
joe-conigliaro <joe@vlang.io>
2020-08-28 16:05:27 +1000

    [v:master] 5526954 - vdoc: cleanup warnings on
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "d80843b"
+#define V_COMMIT_HASH "5526954"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "0975f3b"
+	#define V_COMMIT_HASH "d80843b"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "d80843b"
+	#define V_CURRENT_COMMIT_HASH "5526954"
 #endif
 
 // V typedefs:
