2fd7e15d97b1af232502529fc3615213ac7a785d
joe-conigliaro <joe@vlang.io>
2020-08-29 19:05:27 +1000

    [v:master] d88e8bb - doc: document comma in match
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "2b545f6"
+#define V_COMMIT_HASH "d88e8bb"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "718a469"
+	#define V_COMMIT_HASH "2b545f6"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "2b545f6"
+	#define V_CURRENT_COMMIT_HASH "d88e8bb"
 #endif
 
 // V typedefs:
