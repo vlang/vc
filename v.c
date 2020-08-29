f806f0aa68c347565c7e4d0fbe2cbd72c6994fa9
joe-conigliaro <joe@vlang.io>
2020-08-29 14:50:28 +1000

    [v:master] 718a469 - os: enable test_write_and_read_bytes again
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "fe8286c"
+#define V_COMMIT_HASH "718a469"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "1bf7e15"
+	#define V_COMMIT_HASH "fe8286c"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "fe8286c"
+	#define V_CURRENT_COMMIT_HASH "718a469"
 #endif
 
 // V typedefs:
