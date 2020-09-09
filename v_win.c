8b3c9cbcec1dec3489e307ba0713b538867a4c38
joe-conigliaro <joe@vlang.io>
2020-09-09 22:20:26 +1000

    [v:master] cb43084 - regex: fix compilation
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "ea31f75"
+#define V_COMMIT_HASH "cb43084"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "925f178"
+	#define V_COMMIT_HASH "ea31f75"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "ea31f75"
+	#define V_CURRENT_COMMIT_HASH "cb43084"
 #endif
 
 // V typedefs:
