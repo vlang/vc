2d2098ce13ccba72a6dd937822056aad66fc751b
joe-conigliaro <joe@vlang.io>
2020-09-09 22:15:27 +1000

    [v:master] ea31f75 - checker: more default field fixes
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "925f178"
+#define V_COMMIT_HASH "ea31f75"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "e018509"
+	#define V_COMMIT_HASH "925f178"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "925f178"
+	#define V_CURRENT_COMMIT_HASH "ea31f75"
 #endif
 
 // V typedefs:
