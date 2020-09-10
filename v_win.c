66344ea082591d0c8d147228147585c239750da7
joe-conigliaro <joe@vlang.io>
2020-09-10 21:45:27 +1000

    [v:master] 67cc515 - autofree: ?string test
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "03327e5"
+#define V_COMMIT_HASH "67cc515"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "368c2a6"
+	#define V_COMMIT_HASH "03327e5"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "03327e5"
+	#define V_CURRENT_COMMIT_HASH "67cc515"
 #endif
 
 // V typedefs:
