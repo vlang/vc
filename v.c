caedda126a03b39cee6b8b2392fb9d922a940227
joe-conigliaro <joe@vlang.io>
2020-09-11 20:20:27 +1000

    [v:master] 4aaeaa4 - examples/2048: some updates and improvements (#6343)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "17882db"
+#define V_COMMIT_HASH "4aaeaa4"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "ce6d275"
+	#define V_COMMIT_HASH "17882db"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "17882db"
+	#define V_CURRENT_COMMIT_HASH "4aaeaa4"
 #endif
 
 // V typedefs:
