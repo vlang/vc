5c1a82039314d2aaa1fb94fbf358a1d0177e66aa
joe-conigliaro <joe@vlang.io>
2020-09-11 23:05:27 +1000

    [v:master] d46c1f0 - tests: fix sporadic waitgroup failure on slow windows VMs in anon_fn_test.v
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "ce62f99"
+#define V_COMMIT_HASH "d46c1f0"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "62a692b"
+	#define V_COMMIT_HASH "ce62f99"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "ce62f99"
+	#define V_CURRENT_COMMIT_HASH "d46c1f0"
 #endif
 
 // V typedefs:
