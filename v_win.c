e775830ad067db10d2e81320608bfae7107b47fd
joe-conigliaro <joe@vlang.io>
2020-09-12 21:15:26 +1000

    [v:master] f162e61 - doc: reserved keywords update based on token.v (#6050)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "3731188"
+#define V_COMMIT_HASH "f162e61"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "0801f88"
+	#define V_COMMIT_HASH "3731188"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "3731188"
+	#define V_CURRENT_COMMIT_HASH "f162e61"
 #endif
 
 // V typedefs:
