96565919576f0ffad16af2062fab56dba7dc8911
joe-conigliaro <joe@vlang.io>
2020-09-17 09:05:26 +1000

    [v:master] a984f9c - vup: add quotes for the people with weird V paths (#6393)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "6f7bb30"
+#define V_COMMIT_HASH "a984f9c"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "80b150d"
+	#define V_COMMIT_HASH "6f7bb30"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "6f7bb30"
+	#define V_CURRENT_COMMIT_HASH "a984f9c"
 #endif
 
 // V typedefs:
