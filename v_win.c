0970a36a5f51a354c9ab5c73cf3b9f7e8a020382
joe-conigliaro <joe@vlang.io>
2020-09-20 21:40:28 +1000

    [v:master] 6e5d507 - doctor: retrieve more OS details on mac/linx/windows
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "79ac431"
+#define V_COMMIT_HASH "6e5d507"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "d4fbf42"
+	#define V_COMMIT_HASH "79ac431"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "79ac431"
+	#define V_CURRENT_COMMIT_HASH "6e5d507"
 #endif
 
 // V typedefs:
