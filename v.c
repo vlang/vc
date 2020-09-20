a885db24d6590529a29c86dd2419c866c14f9015
joe-conigliaro <joe@vlang.io>
2020-09-20 22:15:27 +1000

    [v:master] fc19565 - doctor: try lsb_release on linux if it exists, fallback to os.uname if not
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "b39b195"
+#define V_COMMIT_HASH "fc19565"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "6e5d507"
+	#define V_COMMIT_HASH "b39b195"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "b39b195"
+	#define V_CURRENT_COMMIT_HASH "fc19565"
 #endif
 
 // V typedefs:
