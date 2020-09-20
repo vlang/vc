5ae6ce47b9bdc40b316877271dce749168df2ea1
joe-conigliaro <joe@vlang.io>
2020-09-21 00:25:27 +1000

    [v:master] 3454677 - doc: update channel documentation (#6432)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "d7fee91"
+#define V_COMMIT_HASH "3454677"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "fc19565"
+	#define V_COMMIT_HASH "d7fee91"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "d7fee91"
+	#define V_CURRENT_COMMIT_HASH "3454677"
 #endif
 
 // V typedefs:
