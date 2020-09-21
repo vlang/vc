7ccdeeef9426bc83592f7cfbdd2540df7606eb92
joe-conigliaro <joe@vlang.io>
2020-09-22 04:50:26 +1000

    [v:master] f66dcbe - doctor: report tcc location/branch/commit too
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "61330c6"
+#define V_COMMIT_HASH "f66dcbe"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "a9ef7d1"
+	#define V_COMMIT_HASH "61330c6"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "61330c6"
+	#define V_CURRENT_COMMIT_HASH "f66dcbe"
 #endif
 
 // V typedefs:
