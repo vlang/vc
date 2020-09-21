8066aebfde9c7ab3fec4f36fb105e6ff56392b2d
joe-conigliaro <joe@vlang.io>
2020-09-22 05:40:27 +1000

    [v:master] fe8cf19 - doctor: protect against malformed /etc/os-release files on linux
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "f66dcbe"
+#define V_COMMIT_HASH "fe8cf19"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "61330c6"
+	#define V_COMMIT_HASH "f66dcbe"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "f66dcbe"
+	#define V_CURRENT_COMMIT_HASH "fe8cf19"
 #endif
 
 // V typedefs:
