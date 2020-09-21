dddf330d2cde6ad88b4a31ac7169b45a73711f4f
joe-conigliaro <joe@vlang.io>
2020-09-22 04:25:30 +1000

    [v:master] 61330c6 - doctor: support arm64 phones
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "a9ef7d1"
+#define V_COMMIT_HASH "61330c6"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "744f685"
+	#define V_COMMIT_HASH "a9ef7d1"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "a9ef7d1"
+	#define V_CURRENT_COMMIT_HASH "61330c6"
 #endif
 
 // V typedefs:
