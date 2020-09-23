506e9171320a132241a46636aa49f3451f02e61c
joe-conigliaro <joe@vlang.io>
2020-09-24 06:25:27 +1000

    [v:master] 72ecc22 - fmt: support  expressions (#6460)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "4f09ddc"
+#define V_COMMIT_HASH "72ecc22"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "2886b1c"
+	#define V_COMMIT_HASH "4f09ddc"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "4f09ddc"
+	#define V_CURRENT_COMMIT_HASH "72ecc22"
 #endif
 
 // V typedefs:
