c5bf37bae925b41d9731cbbb985173915c2af918
joe-conigliaro <joe@vlang.io>
2020-09-24 23:55:26 +1000

    [v:master] 1f21bc4 - gg: bugfix on missing font file, remove last empty line in
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "d91594d"
+#define V_COMMIT_HASH "1f21bc4"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "48d8711"
+	#define V_COMMIT_HASH "d91594d"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "d91594d"
+	#define V_CURRENT_COMMIT_HASH "1f21bc4"
 #endif
 
 // V typedefs:
