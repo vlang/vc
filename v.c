c6f98c2ecd5c1096339efc739b8f749d193627eb
joe-conigliaro <joe@vlang.io>
2020-09-25 00:10:27 +1000

    [v:master] dbce017 - gg: try 3 different fonts in system_font_path on MacOS
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "1f21bc4"
+#define V_COMMIT_HASH "dbce017"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "d91594d"
+	#define V_COMMIT_HASH "1f21bc4"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "1f21bc4"
+	#define V_CURRENT_COMMIT_HASH "dbce017"
 #endif
 
 // V typedefs:
