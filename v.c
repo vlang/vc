bb6fc2323152dde18009c06ee347686334154574
joe-conigliaro <joe@vlang.io>
2020-09-24 23:35:27 +1000

    [v:master] 48d8711 - doctor: limit horizontal divider to just 20 dashes, to avoid wraps
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "3925c3f"
+#define V_COMMIT_HASH "48d8711"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "72ecc22"
+	#define V_COMMIT_HASH "3925c3f"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "3925c3f"
+	#define V_CURRENT_COMMIT_HASH "48d8711"
 #endif
 
 // V typedefs:
