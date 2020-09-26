cec310b30e27c4b7ee791bcb316c89f3033d61ed
joe-conigliaro <joe@vlang.io>
2020-09-26 16:40:27 +1000

    [v:master] af37c7a - gg: prevent messages about missing font variants without
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "d782de5"
+#define V_COMMIT_HASH "af37c7a"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "b999d01"
+	#define V_COMMIT_HASH "d782de5"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "d782de5"
+	#define V_CURRENT_COMMIT_HASH "af37c7a"
 #endif
 
 // V typedefs:
