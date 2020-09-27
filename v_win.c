3925bc495cc7074ab8cbd75c9f93d963e55daba8
joe-conigliaro <joe@vlang.io>
2020-09-27 18:20:26 +1000

    [v:master] 03258db - ci: other mut vlib test fixes
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "aa889b0"
+#define V_COMMIT_HASH "03258db"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "7c66953"
+	#define V_COMMIT_HASH "aa889b0"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "aa889b0"
+	#define V_CURRENT_COMMIT_HASH "03258db"
 #endif
 
 // V typedefs:
