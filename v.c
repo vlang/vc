cc078453cad7be8ac7c228e159e915f610651877
joe-conigliaro <joe@vlang.io>
2020-09-28 00:00:28 +1000

    [v:master] 7305b2c - ci: fix for mut arg in cmd/tools/vdoc.v
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "a6d525e"
+#define V_COMMIT_HASH "7305b2c"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "3a869c7"
+	#define V_COMMIT_HASH "a6d525e"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "a6d525e"
+	#define V_CURRENT_COMMIT_HASH "7305b2c"
 #endif
 
 // V typedefs:
