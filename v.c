d389cf89d1ea3e86328bde86bb298b8b419f6d81
joe-conigliaro <joe@vlang.io>
2020-10-02 04:10:27 +1000

    [v:master] d96f1bf - cmd/tools/repeat.v: add and fill a new map[string][]int field CmdResult.oms
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "9dc75f5"
+#define V_COMMIT_HASH "d96f1bf"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "0402179"
+	#define V_COMMIT_HASH "9dc75f5"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "9dc75f5"
+	#define V_CURRENT_COMMIT_HASH "d96f1bf"
 #endif
 
 // V typedefs:
