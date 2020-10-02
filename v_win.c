126499a691118aa03ea4055c7b262e1361681de7
joe-conigliaro <joe@vlang.io>
2020-10-02 20:30:28 +1000

    [v:master] c53ebd8 - repeat.v: implement a --series option
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "032a3e6"
+#define V_COMMIT_HASH "c53ebd8"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "70c8713"
+	#define V_COMMIT_HASH "032a3e6"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "032a3e6"
+	#define V_CURRENT_COMMIT_HASH "c53ebd8"
 #endif
 
 // V typedefs:
