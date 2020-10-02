0002b9a61adc9f1c6f7bc232e9d294646650b657
joe-conigliaro <joe@vlang.io>
2020-10-02 19:50:28 +1000

    [v:master] 032a3e6 - ci: add new performance-regressions job, using cmd/tools/repeat
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "70c8713"
+#define V_COMMIT_HASH "032a3e6"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "c747ba1"
+	#define V_COMMIT_HASH "70c8713"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "70c8713"
+	#define V_CURRENT_COMMIT_HASH "032a3e6"
 #endif
 
 // V typedefs:
