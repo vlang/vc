812cf9d3a4a09ab9c774e56cad4042c74a51f859
joe-conigliaro <joe@vlang.io>
2020-10-02 18:00:27 +1000

    [v:master] c747ba1 - repeat.v: add --fail_percent option
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "7c86b03"
+#define V_COMMIT_HASH "c747ba1"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "94cf8ef"
+	#define V_COMMIT_HASH "7c86b03"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "7c86b03"
+	#define V_CURRENT_COMMIT_HASH "c747ba1"
 #endif
 
 // V typedefs:
