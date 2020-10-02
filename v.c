a418b7729af5bf6463ebbe4dfae23ccd86cad4d7
joe-conigliaro <joe@vlang.io>
2020-10-02 18:40:30 +1000

    [v:master] 70c8713 - live_test.v: wait 5 times more on macos
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "c747ba1"
+#define V_COMMIT_HASH "70c8713"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "7c86b03"
+	#define V_COMMIT_HASH "c747ba1"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "c747ba1"
+	#define V_CURRENT_COMMIT_HASH "70c8713"
 #endif
 
 // V typedefs:
