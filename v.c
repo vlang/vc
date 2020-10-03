35c2b3bfab7d29a5e42b3439c0df201bfc9c8271
joe-conigliaro <joe@vlang.io>
2020-10-04 00:45:27 +1000

    [v:master] d93b0f0 - vdoc: return platform specific prefixes from .vdocignore files
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "c324169"
+#define V_COMMIT_HASH "d93b0f0"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "7e13518"
+	#define V_COMMIT_HASH "c324169"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "c324169"
+	#define V_CURRENT_COMMIT_HASH "d93b0f0"
 #endif
 
 // V typedefs:
