6162e66dd3043a5a4ff1d812ac6e2efd1236fbc9
joe-conigliaro <joe@vlang.io>
2020-10-03 22:10:27 +1000

    [v:master] 5aea0d0 - docs: add bitwise not (~) to the list of int operators
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "a6071da"
+#define V_COMMIT_HASH "5aea0d0"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "de77f0b"
+	#define V_COMMIT_HASH "a6071da"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "a6071da"
+	#define V_CURRENT_COMMIT_HASH "5aea0d0"
 #endif
 
 // V typedefs:
