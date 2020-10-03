24f0f3ae9a0b20e9f6f4397eac94ee089d7cdce9
joe-conigliaro <joe@vlang.io>
2020-10-03 20:20:27 +1000

    [v:master] a6071da - makefile: abstract V exe extension for better coherence (#6544)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "de77f0b"
+#define V_COMMIT_HASH "a6071da"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "677b0ba"
+	#define V_COMMIT_HASH "de77f0b"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "de77f0b"
+	#define V_CURRENT_COMMIT_HASH "a6071da"
 #endif
 
 // V typedefs:
