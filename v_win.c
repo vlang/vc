afe596c2699bfd9816cd40976b61900750c3470b
joe-conigliaro <joe@vlang.io>
2020-10-06 16:15:27 +1100

    [v:master] f943bf2 - repeat.v: merge results from all series runs for the totals
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "256a572"
+#define V_COMMIT_HASH "f943bf2"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "ed15b40"
+	#define V_COMMIT_HASH "256a572"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "256a572"
+	#define V_CURRENT_COMMIT_HASH "f943bf2"
 #endif
 
 // V typedefs:
