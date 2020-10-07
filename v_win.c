872db9fb468850be89763bfdf585b7ee8be850df
joe-conigliaro <joe@vlang.io>
2020-10-08 03:20:29 +1100

    [v:master] 393889a - readme: minor clarification
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "efce448"
+#define V_COMMIT_HASH "393889a"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "d46d607"
+	#define V_COMMIT_HASH "efce448"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "efce448"
+	#define V_CURRENT_COMMIT_HASH "393889a"
 #endif
 
 // V comptime_defines:
