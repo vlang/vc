7db73d690962f4e1e49b0341c816734fb6cd5bab
joe-conigliaro <joe@vlang.io>
2020-10-09 19:10:28 +1100

    [v:master] 0edcd5e - repeat.v: support --max_time limit for the average time of a command
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "c463c26"
+#define V_COMMIT_HASH "0edcd5e"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "92630a2"
+	#define V_COMMIT_HASH "c463c26"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "c463c26"
+	#define V_CURRENT_COMMIT_HASH "0edcd5e"
 #endif
 
 // V comptime_defines:
