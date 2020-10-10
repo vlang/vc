1a283358a98d647eba378c3ef998e1767aa62352
joe-conigliaro <joe@vlang.io>
2020-10-11 03:40:28 +1100

    [v:master] 86816b1 - examples: add bytebeat melody using sokol.audio
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "b677ad9"
+#define V_COMMIT_HASH "86816b1"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "6038264"
+	#define V_COMMIT_HASH "b677ad9"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "b677ad9"
+	#define V_CURRENT_COMMIT_HASH "86816b1"
 #endif
 
 // V comptime_defines:
