499e493501ba215903b2f66b098937c4bbeb05ce
joe-conigliaro <joe@vlang.io>
2020-10-11 07:50:27 +1100

    [v:master] 77c29c6 - examples: add simple visual rendering of the samples in melody.v
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "86816b1"
+#define V_COMMIT_HASH "77c29c6"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "b677ad9"
+	#define V_COMMIT_HASH "86816b1"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "86816b1"
+	#define V_CURRENT_COMMIT_HASH "77c29c6"
 #endif
 
 // V comptime_defines:
