25c8254d880825f24ba14fc007e68fba07934f9a
joe-conigliaro <joe@vlang.io>
2020-10-10 19:40:27 +1100

    [v:master] d8d80fb - gg: add a missing math import and vfmt
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "1332bba"
+#define V_COMMIT_HASH "d8d80fb"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "b2fdd7f"
+	#define V_COMMIT_HASH "1332bba"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "1332bba"
+	#define V_CURRENT_COMMIT_HASH "d8d80fb"
 #endif
 
 // V comptime_defines:
