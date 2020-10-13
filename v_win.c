900b7e5b6edf40022954e9ce48680ca5d55ee293
joe-conigliaro <joe@vlang.io>
2020-10-14 06:05:28 +1100

    [v:master] a2fba05 - fmt: allow fmt to be run on directories (#6613)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "ee0baf6"
+#define V_COMMIT_HASH "a2fba05"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "194c2c9"
+	#define V_COMMIT_HASH "ee0baf6"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "ee0baf6"
+	#define V_CURRENT_COMMIT_HASH "a2fba05"
 #endif
 
 // V comptime_defines:
