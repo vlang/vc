0f7eccd2f66839cbdea47f85a525368cc4a1db5d
joe-conigliaro <joe@vlang.io>
2020-10-13 21:20:30 +1100

    [v:master] 05ec32c - examples: fix sokol/particles to start up faster (#6610)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "28dea17"
+#define V_COMMIT_HASH "05ec32c"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "57d705e"
+	#define V_COMMIT_HASH "28dea17"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "28dea17"
+	#define V_CURRENT_COMMIT_HASH "05ec32c"
 #endif
 
 // V comptime_defines:
