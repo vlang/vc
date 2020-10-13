9504251b73f2b692996b2d3dfa85b3bfefebbe97
joe-conigliaro <joe@vlang.io>
2020-10-14 00:10:28 +1100

    [v:master] 1123f4f - vfmt: fix deep nested map types, using f.table.type_to_str
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "78bcda1"
+#define V_COMMIT_HASH "1123f4f"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "05ec32c"
+	#define V_COMMIT_HASH "78bcda1"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "78bcda1"
+	#define V_CURRENT_COMMIT_HASH "1123f4f"
 #endif
 
 // V comptime_defines:
