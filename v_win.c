9fae3c91da125f79b6e44c2318c38c667ad9faac
joe-conigliaro <joe@vlang.io>
2020-10-13 17:25:30 +1100

    [v:master] 28dea17 - ci: prevent Periodic failure, ensure VSMTP_TEST_USER/VSMTP_TEST_PASS are set in smtp_test.v
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "57d705e"
+#define V_COMMIT_HASH "28dea17"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "da7d531"
+	#define V_COMMIT_HASH "57d705e"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "57d705e"
+	#define V_CURRENT_COMMIT_HASH "28dea17"
 #endif
 
 // V comptime_defines:
