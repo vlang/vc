4f2944f004f55088efe7a99fe56c1a9482ae2763
joe-conigliaro <joe@vlang.io>
2020-10-15 07:50:27 +1100

    [v:master] 5dd34c3 - v test-fmt: skip os.v, linuxsys_bare.v, life.v for now
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "5a11b87"
+#define V_COMMIT_HASH "5dd34c3"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "9e16576"
+	#define V_COMMIT_HASH "5a11b87"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "5a11b87"
+	#define V_CURRENT_COMMIT_HASH "5dd34c3"
 #endif
 
 // V comptime_defines:
