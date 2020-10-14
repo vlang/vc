3f2d1190705310ece8296995dd2630c4a8aac4f9
joe-conigliaro <joe@vlang.io>
2020-10-15 02:25:28 +1100

    [v:master] e02f25a - test-compiler: fix sporadic fails for vpm/v install
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "d1e5262"
+#define V_COMMIT_HASH "e02f25a"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "9355c60"
+	#define V_COMMIT_HASH "d1e5262"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "d1e5262"
+	#define V_CURRENT_COMMIT_HASH "e02f25a"
 #endif
 
 // V comptime_defines:
