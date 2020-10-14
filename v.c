f88985e88bdc55379b1e058103d0b73b853716eb
joe-conigliaro <joe@vlang.io>
2020-10-15 08:00:29 +1100

    [v:master] e36f117 - v test-fmt: use .skip_files instead of filtering
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "5dd34c3"
+#define V_COMMIT_HASH "e36f117"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "5a11b87"
+	#define V_COMMIT_HASH "5dd34c3"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "5dd34c3"
+	#define V_CURRENT_COMMIT_HASH "e36f117"
 #endif
 
 // V comptime_defines:
