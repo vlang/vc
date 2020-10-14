f99051af11d5abbfd5c71020533ab48e81b5bee5
joe-conigliaro <joe@vlang.io>
2020-10-15 01:35:30 +1100

    [v:master] d1e5262 - crypto.hmac: implement hmac.equal/2 too
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "9355c60"
+#define V_COMMIT_HASH "d1e5262"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "25947a7"
+	#define V_COMMIT_HASH "9355c60"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "9355c60"
+	#define V_CURRENT_COMMIT_HASH "d1e5262"
 #endif
 
 // V comptime_defines:
