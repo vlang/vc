a99cf0053c7291662a74fa85716851797a5c404f
joe-conigliaro <joe@vlang.io>
2020-10-15 19:45:27 +1100

    [v:master] 44a42b7 - ci: fix parser.v formatting
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "effa93d"
+#define V_COMMIT_HASH "44a42b7"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "6326b6d"
+	#define V_COMMIT_HASH "effa93d"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "effa93d"
+	#define V_CURRENT_COMMIT_HASH "44a42b7"
 #endif
 
 // V comptime_defines:
