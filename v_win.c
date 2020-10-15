9fff821f672cbea6a50977da5ca2d47a0a672631
joe-conigliaro <joe@vlang.io>
2020-10-15 23:45:28 +1100

    [v:master] 5d4cce3 - ci: fix failing tests
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "8b13f3b"
+#define V_COMMIT_HASH "5d4cce3"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "3d4ab7d"
+	#define V_COMMIT_HASH "8b13f3b"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "8b13f3b"
+	#define V_CURRENT_COMMIT_HASH "5d4cce3"
 #endif
 
 // V comptime_defines:
