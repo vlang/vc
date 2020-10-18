440994d66814cb8a5f872f202268be46beb2e100
joe-conigliaro <joe@vlang.io>
2020-10-18 17:05:27 +1100

    [v:master] b7233a2 - examples: build examples/path_tracing.v with -prod, with no warnings/errors
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "5c4ba34"
+#define V_COMMIT_HASH "b7233a2"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "55536bb"
+	#define V_COMMIT_HASH "5c4ba34"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "5c4ba34"
+	#define V_CURRENT_COMMIT_HASH "b7233a2"
 #endif
 
 // V comptime_defines:
