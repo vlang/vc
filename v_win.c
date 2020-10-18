31fd06d3cbb3226284726ec46568dc67b8da040e
joe-conigliaro <joe@vlang.io>
2020-10-18 17:35:28 +1100

    [v:master] 67ecc04 - examples: build examples/hello_v_js.v without warnings/errors
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "b7233a2"
+#define V_COMMIT_HASH "67ecc04"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "5c4ba34"
+	#define V_COMMIT_HASH "b7233a2"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "b7233a2"
+	#define V_CURRENT_COMMIT_HASH "67ecc04"
 #endif
 
 // V comptime_defines:
