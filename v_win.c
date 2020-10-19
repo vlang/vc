5db8cd309cd1891fdba7c2ff36e7c6d8fbe249f3
joe-conigliaro <joe@vlang.io>
2020-10-20 06:35:28 +1100

    [v:master] 3ae3196 - vdoc: allow to omit timestamp (#6652)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "f37e936"
+#define V_COMMIT_HASH "3ae3196"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "37d6503"
+	#define V_COMMIT_HASH "f37e936"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "f37e936"
+	#define V_CURRENT_COMMIT_HASH "3ae3196"
 #endif
 
 // V comptime_defines:
