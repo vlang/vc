56cace93932c696a0e91ee521612470a6a84dd6f
joe-conigliaro <joe@vlang.io>
2020-10-21 19:10:27 +1100

    [v:master] 0e56b96 - v test-fmt: recompile vfmt only when needed
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "09128ac"
+#define V_COMMIT_HASH "0e56b96"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "c4fa1db"
+	#define V_COMMIT_HASH "09128ac"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "09128ac"
+	#define V_CURRENT_COMMIT_HASH "0e56b96"
 #endif
 
 // V comptime_defines:
