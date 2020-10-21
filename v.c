8cc1018b82b2562da8ee6564c142de17ba551fe6
joe-conigliaro <joe@vlang.io>
2020-10-21 18:15:27 +1100

    [v:master] 09128ac - ci: fix  case
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "c4fa1db"
+#define V_COMMIT_HASH "09128ac"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "f096988"
+	#define V_COMMIT_HASH "c4fa1db"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "c4fa1db"
+	#define V_CURRENT_COMMIT_HASH "09128ac"
 #endif
 
 // V comptime_defines:
