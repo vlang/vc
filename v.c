310db482a715e0008d6c2d7b04ddafd646bb55e5
joe-conigliaro <joe@vlang.io>
2020-10-26 00:00:28 +1100

    [v:master] 8e478e8 - doc: minor typo/English improvements (#6678)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "60cc8ac"
+#define V_COMMIT_HASH "8e478e8"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "5cb31c2"
+	#define V_COMMIT_HASH "60cc8ac"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "60cc8ac"
+	#define V_CURRENT_COMMIT_HASH "8e478e8"
 #endif
 
 // V comptime_defines:
