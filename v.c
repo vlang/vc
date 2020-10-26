2cbe5dd3258e796f470fd562b6406811e7f03363
joe-conigliaro <joe@vlang.io>
2020-10-26 23:45:28 +1100

    [v:master] 0c192cf - tests: fix warnings when doing
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "9772eb7"
+#define V_COMMIT_HASH "0c192cf"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "a7e3092"
+	#define V_COMMIT_HASH "9772eb7"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "9772eb7"
+	#define V_CURRENT_COMMIT_HASH "0c192cf"
 #endif
 
 // V comptime_defines:
