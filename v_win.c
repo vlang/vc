31f339cfdf3d5e1ec08c213d2831ced3f298c796
joe-conigliaro <joe@vlang.io>
2020-11-04 19:20:28 +1100

    [v:master] cc4bb71 - os: prevent unnecessary dependencies when importing  on windows (#6741)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "cd399b7"
+#define V_COMMIT_HASH "cc4bb71"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "518da92"
+	#define V_COMMIT_HASH "cd399b7"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "cd399b7"
+	#define V_CURRENT_COMMIT_HASH "cc4bb71"
 #endif
 
 // V comptime_defines:
@@ -742,20 +742,6 @@ voidptr memfreedup(voidptr ptr, voidptr src, int sz) {
 
 // added by module `os`:
 #include <errno.h>
-// added by module `os`:
-
-#if defined(__has_include)
-
-#if __has_include(<winsock2.h>)
-#include <winsock2.h>
-#else
-#error VERROR_MESSAGE Header file <winsock2.h>, needed for module `os` was not found. Please install the corresponding development headers.
-#endif
-
-#else
-#include <winsock2.h>
-#endif
-
 // added by module `os`:
 
 #if defined(__has_include)
@@ -18882,7 +18868,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("518da92"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("cd399b7"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 static string v__pref__default_c_compiler() {
