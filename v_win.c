bfbb0bd7fd8a51b6e6d53b25115473583d0b200c
joe-conigliaro <joe@vlang.io>
2020-11-09 03:50:29 +1100

    [v:master] 68cfbd6 - websocket: fix autobahn test to always use latest v(#6780)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "2994e71"
+#define V_COMMIT_HASH "68cfbd6"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "6da8454"
+	#define V_COMMIT_HASH "2994e71"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "2994e71"
+	#define V_CURRENT_COMMIT_HASH "68cfbd6"
 #endif
 
 // V comptime_defines:
@@ -18993,7 +18993,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("6da8454"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("2994e71"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 static string v__pref__default_c_compiler() {
@@ -19298,6 +19298,9 @@ multi_return_v__pref__Preferences_string v__pref__parse_args(array_string args) 
 			i++;
 		} else if (string_eq(_t556, tos_lit("-o"))) {
 			res->out_name = os__cmdline__option(current_args, tos_lit("-o"), tos_lit(""));
+			if (string_ends_with(res->out_name, tos_lit(".js"))) {
+				res->backend = v__pref__Backend_js;
+			}
 			i++;
 		} else if (string_eq(_t556, tos_lit("-b"))) {
 			string sbackend = os__cmdline__option(current_args, tos_lit("-b"), tos_lit("c"));
@@ -45559,7 +45562,11 @@ void v__builder__Builder_compile_js(v__builder__Builder* b) {
 		println(tos_lit("all .v files:"));
 		println(array_string_str(files));
 	}
-	v__builder__Builder_build_js(b, files, string_add(b->pref->out_name, tos_lit(".js")));
+	string name = b->pref->out_name;
+	if (!string_ends_with(name, tos_lit(".js"))) {
+		name = /*f*/string_add(name, tos_lit(".js"));
+	}
+	v__builder__Builder_build_js(b, files, name);
 }
 
 static void v__builder__Builder_run_js(v__builder__Builder* b) {
