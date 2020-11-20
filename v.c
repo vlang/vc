f939a49caef85790157e28c66456a5a7b83beb9a
joe-conigliaro <joe@vlang.io>
2020-11-20 11:45:29 +1100

    [v:master] 1b8609c - builder: fix for
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "a67873f"
+#define V_COMMIT_HASH "1b8609c"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "d50c919"
+	#define V_COMMIT_HASH "a67873f"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "a67873f"
+	#define V_CURRENT_COMMIT_HASH "1b8609c"
 #endif
 
 // V comptime_defines:
@@ -19666,7 +19666,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("d50c919"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("a67873f"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 static string v__pref__default_c_compiler() {
@@ -46866,9 +46866,11 @@ static void v__builder__Builder_run_compiled_executable_and_exit(v__builder__Bui
 		string bundle_id = ((b->pref->bundle_id).len != 0 ? (b->pref->bundle_id) : (_STR("app.vlang.%.*s", 1, bundle_name)));
 		os__exec(_STR("xcrun simctl launch %.*s\000 %.*s", 2, device, bundle_id));
 	} else {
-		string cmd = _STR("\"%.*s\000\"", 2, b->pref->out_name);
+		string exefile = os__real_path(b->pref->out_name);
+		string cmd = _STR("\"%.*s\000\"", 2, exefile);
 		if (b->pref->backend == v__pref__Backend_js) {
-			cmd = _STR("node \"%.*s\000.js\"", 2, b->pref->out_name);
+			string jsfile = os__real_path(_STR("%.*s\000.js", 2, b->pref->out_name));
+			cmd = _STR("node \"%.*s\000\"", 2, jsfile);
 		}
 		// FOR IN array
 		array _t1908 = b->pref->run_args;
