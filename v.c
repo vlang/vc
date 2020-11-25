71a357b07ec085851f688ab2d60ffe896e685367
joe-conigliaro <joe@vlang.io>
2020-11-25 22:20:29 +1100

    [v:master] a46eda7 - builder: fix dll generation on Windows (#6939)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "7d6f972"
+#define V_COMMIT_HASH "a46eda7"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "96b73ac"
+	#define V_COMMIT_HASH "7d6f972"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "7d6f972"
+	#define V_CURRENT_COMMIT_HASH "a46eda7"
 #endif
 
 // V comptime_defines:
@@ -21545,7 +21545,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("96b73ac"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("7d6f972"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 static string v__pref__default_c_compiler() {
@@ -45188,6 +45188,10 @@ static void v__builder__Builder_cc(v__builder__Builder* v) {
 		#if defined(__APPLE__)
 		{
 			v->pref->out_name = /*f*/string_add(v->pref->out_name, tos_lit(".dylib"));
+		}
+		#elif defined(_WIN32)
+		{
+			v->pref->out_name = /*f*/string_add(v->pref->out_name, tos_lit(".dll"));
 		}
 		#else
 		{
