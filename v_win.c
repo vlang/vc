641e91a48730767d27e48d2af0158a4e003fc28e
joe-conigliaro <joe@vlang.io>
2020-11-26 06:45:30 +1100

    [v:master] 2957541 - pref: fix message on inferred run (#6947)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "fa126b9"
+#define V_COMMIT_HASH "2957541"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "4e9967a"
+	#define V_COMMIT_HASH "fa126b9"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "fa126b9"
+	#define V_CURRENT_COMMIT_HASH "2957541"
 #endif
 
 // V comptime_defines:
@@ -21403,7 +21403,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("4e9967a"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("fa126b9"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 VV_LOCAL_SYMBOL string v__pref__default_c_compiler() {
@@ -21836,7 +21836,7 @@ multi_return_v__pref__Preferences_string v__pref__parse_args(array_string args) 
 		}
 		v__pref__must_exist(res->path);
 		if (!string_ends_with(res->path, tos_lit(".v")) && os__is_executable(res->path) && os__is_file(res->path) && os__is_file(string_add(res->path, tos_lit(".v")))) {
-			eprintln(_STR("It looks like you wanted to run `v %.*s\000`, so we went ahead and did that since \"%.*s\000\" is an executable.", 3, res->path, res->path));
+			eprintln(_STR("It looks like you wanted to run \"%.*s\000.v\", so we went ahead and did that since \"%.*s\000\" is an executable.", 3, res->path, res->path));
 			res->path = /*f*/string_add(res->path, tos_lit(".v"));
 		}
 	}
