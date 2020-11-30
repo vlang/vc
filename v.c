24de15acec45f07bd94472e841dd4cca10c64a9a
joe-conigliaro <joe@vlang.io>
2020-12-01 03:50:31 +1100

    [v:master] 376833a - builder: show tcc compiler error with -v and -showcc
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "434fa70"
+#define V_COMMIT_HASH "376833a"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "a981621"
+	#define V_COMMIT_HASH "434fa70"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "434fa70"
+	#define V_CURRENT_COMMIT_HASH "376833a"
 #endif
 
 // V comptime_defines:
@@ -20576,7 +20576,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("a981621"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("434fa70"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 VV_LOCAL_SYMBOL void v__pref__Preferences_try_to_use_tcc_by_default(v__pref__Preferences* p) {
@@ -45947,7 +45947,7 @@ VV_LOCAL_SYMBOL void v__builder__Builder_cc(v__builder__Builder* v) {
  	os__Result res = *(os__Result*) _t1865.data;
 	i64 diff = time__ticks() - ticks;
 	v__builder__Builder_timing_message(v, _STR("C %*.*s", 1, ccompiler, 3), diff);
-	if (v->pref->show_c_output) {
+	if (v->pref->is_verbose || v->pref->show_c_output || v->pref->show_cc) {
 		v__builder__Builder_show_c_compiler_output(v, res);
 	}
 	os__chdir(original_pwd);
