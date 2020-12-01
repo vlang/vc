9bc3bbb510f1bff2d09e81ed05f154dc0e729286
joe-conigliaro <joe@vlang.io>
2020-12-01 21:45:30 +1100

    [v:master] 6a1e032 - gen: fix interpolation variadic (#7059)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "88835a0"
+#define V_COMMIT_HASH "6a1e032"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "9b583f5"
+	#define V_COMMIT_HASH "88835a0"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "88835a0"
+	#define V_CURRENT_COMMIT_HASH "6a1e032"
 #endif
 
 // V comptime_defines:
@@ -20463,7 +20463,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("9b583f5"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("88835a0"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 VV_LOCAL_SYMBOL void v__pref__Preferences_try_to_use_tcc_by_default(v__pref__Preferences* p) {
@@ -42598,6 +42598,8 @@ VV_LOCAL_SYMBOL void v__gen__Gen_string_inter_literal(v__gen__Gen* g, v__ast__St
 			} else {
 				v__gen__Gen_write(g, tos_lit("*.*s"));
 			}
+		} else if (v__table__Type_has_flag(typ, v__table__TypeFlag_variadic)) {
+			v__gen__Gen_write(g, tos_lit(".*s"));
 		} else if (v__table__Type_is_float(typ)) {
 			v__gen__Gen_write(g, _STR("%.*s\000%c", 2, fmt, fspec));
 		} else if (v__table__Type_is_pointer(typ)) {
@@ -42644,6 +42646,8 @@ VV_LOCAL_SYMBOL void v__gen__Gen_string_inter_literal(v__gen__Gen* g, v__ast__St
 			} else {
 				v__gen__Gen_expr(g, expr);
 			}
+		} else if (v__table__Type_has_flag(typ, v__table__TypeFlag_variadic)) {
+			v__gen__Gen_gen_expr_to_string(g, expr, typ);
 		} else if (typ == _const_v__table__bool_type) {
 			v__gen__Gen_expr(g, expr);
 			v__gen__Gen_write(g, tos_lit(" ? _SLIT(\"true\") : _SLIT(\"false\")"));
