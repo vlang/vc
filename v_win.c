18c34b6b6efb130cdea3bea3dc7959eb7de9bfe3
joe-conigliaro <joe@vlang.io>
2020-12-03 10:00:31 +1100

    [v:master] 1037d3a - checker: make missing  before an argument a warning
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "4b94c29"
+#define V_COMMIT_HASH "1037d3a"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "ce1fa11"
+	#define V_COMMIT_HASH "4b94c29"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "4b94c29"
+	#define V_CURRENT_COMMIT_HASH "1037d3a"
 #endif
 
 // V comptime_defines:
@@ -20440,7 +20440,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("ce1fa11"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("4b94c29"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 VV_LOCAL_SYMBOL void v__pref__Preferences_try_to_use_tcc_by_default(v__pref__Preferences* p) {
@@ -26738,7 +26738,7 @@ v__table__Type v__checker__Checker_call_method(v__checker__Checker* c, v__ast__C
 			} else {
 				if (param.is_mut && (!arg.is_mut || v__table__Type_share(param.typ) != arg.share)) {
 					string tok = v__table__ShareType_str(arg.share);
-					v__checker__Checker_error(c, _STR("`%.*s\000` parameter `%.*s\000` is `%.*s\000`, you need to provide `%.*s\000` e.g. `%.*s\000 arg%"PRId32"\000`", 7, call_expr->name, param.name, tok, tok, tok, i + 1), v__ast__Expr_position(arg.expr));
+					v__checker__Checker_warn(c, _STR("`%.*s\000` parameter `%.*s\000` is `%.*s\000`, you need to provide `%.*s\000` e.g. `%.*s\000 arg%"PRId32"\000`", 7, call_expr->name, param.name, tok, tok, tok, i + 1), v__ast__Expr_position(arg.expr));
 				}
 			}
 		}
@@ -26997,7 +26997,7 @@ v__table__Type v__checker__Checker_call_fn(v__checker__Checker* c, v__ast__CallE
 		} else {
 			if (arg.is_mut && (!call_arg.is_mut || v__table__Type_share(arg.typ) != call_arg.share)) {
 				string tok = v__table__ShareType_str(call_arg.share);
-				v__checker__Checker_error(c, _STR("`%.*s\000` parameter `%.*s\000` is `%.*s\000`, you need to provide `%.*s\000` e.g. `%.*s\000 arg%"PRId32"\000`", 7, call_expr->name, arg.name, tok, tok, tok, i + 1), v__ast__Expr_position(call_arg.expr));
+				v__checker__Checker_warn(c, _STR("`%.*s\000` parameter `%.*s\000` is `%.*s\000`, you need to provide `%.*s\000` e.g. `%.*s\000 arg%"PRId32"\000`", 7, call_expr->name, arg.name, tok, tok, tok, i + 1), v__ast__Expr_position(call_arg.expr));
 			}
 		}
 		if (arg_typ_sym->kind == v__table__Kind_interface_) {
@@ -28703,8 +28703,8 @@ VV_LOCAL_SYMBOL v__table__Type v__checker__Checker_at_expr(v__checker__Checker* 
 	} else if (_t1038 == v__token__AtKind_line_nr) {
 		node->val = int_str((node->pos.line_nr + 1));
 	} else if (_t1038 == v__token__AtKind_column_nr) {
-		multi_return_string_int mr_105461 = v__util__filepath_pos_to_source_and_column(c->file->path, node->pos);
-		int column = mr_105461.arg1;
+		multi_return_string_int mr_105459 = v__util__filepath_pos_to_source_and_column(c->file->path, node->pos);
+		int column = mr_105459.arg1;
 		node->val = int_str((column + 1));
 	} else if (_t1038 == v__token__AtKind_vhash) {
 		node->val = v__util__vhash();
@@ -29630,8 +29630,8 @@ v__table__Type v__checker__Checker_postfix_expr(v__checker__Checker* c, v__ast__
 	if (!v__table__TypeSymbol_is_number(typ_sym)) {
 		v__checker__Checker_error(c, _STR("invalid operation: %.*s\000 (non-numeric type `%.*s\000`)", 3, v__token__Kind_str(node->op), typ_sym->source_name), node->pos);
 	} else {
-		multi_return_string_v__token__Position mr_135121 = v__checker__Checker_fail_if_immutable(c, node->expr);
-		node->auto_locked = mr_135121.arg0;
+		multi_return_string_v__token__Position mr_135119 = v__checker__Checker_fail_if_immutable(c, node->expr);
+		node->auto_locked = mr_135119.arg0;
 	}
 	if ((v__table__Type_is_ptr(typ) || v__table__TypeSymbol_is_pointer(typ_sym)) && !c->inside_unsafe) {
 		v__checker__Checker_warn(c, tos_lit("pointer arithmetic is only allowed in `unsafe` blocks"), node->pos);
@@ -30125,10 +30125,10 @@ VV_LOCAL_SYMBOL void v__checker__Checker_verify_all_vweb_routes(v__checker__Chec
 		for (int _t1138 = 0; _t1138 < _t1137.len; ++_t1138) {
 			v__table__Fn m = ((v__table__Fn*)_t1137.data)[_t1138];
 			if (string_eq(m.return_type_source_name, tos_lit("vweb.Result"))) {
-				multi_return_bool_int_int mr_151053 = v__checker__Checker_verify_vweb_params_for_method(c, m);
-				bool is_ok = mr_151053.arg0;
-				int nroute_attributes = mr_151053.arg1;
-				int nargs = mr_151053.arg2;
+				multi_return_bool_int_int mr_151051 = v__checker__Checker_verify_vweb_params_for_method(c, m);
+				bool is_ok = mr_151051.arg0;
+				int nroute_attributes = mr_151051.arg1;
+				int nargs = mr_151051.arg2;
 				if (!is_ok) {
 					v__ast__FnDecl* f = ((v__ast__FnDecl*)(m.source_fn));
 					if (isnil(f)) {
