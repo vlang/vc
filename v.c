c8563a692209ee65c0732a146ba7dd9bb55e51c4
joe-conigliaro <joe@vlang.io>
2020-12-03 11:05:35 +1100

    [v:master] 30da85a - checker: fix optional or_block {none} (#7095)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "c1b25dd"
+#define V_COMMIT_HASH "30da85a"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "1037d3a"
+	#define V_COMMIT_HASH "c1b25dd"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "c1b25dd"
+	#define V_CURRENT_COMMIT_HASH "30da85a"
 #endif
 
 // V comptime_defines:
@@ -20592,7 +20592,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("1037d3a"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){tos_lit("c1b25dd"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 VV_LOCAL_SYMBOL void v__pref__Preferences_try_to_use_tcc_by_default(v__pref__Preferences* p) {
@@ -25459,7 +25459,7 @@ bool v__checker__Checker_check_basic(v__checker__Checker* c, v__table__Type got,
 		return true;
 	}
 	if (got_idx == _const_v__table__none_type_idx && v__table__Type_has_flag(expected, v__table__TypeFlag_optional)) {
-		return true;
+		return false;
 	}
 	if (exp_is_ptr && got_idx == _const_v__table__int_type_idx) {
 		return true;
@@ -25592,10 +25592,10 @@ VV_LOCAL_SYMBOL v__table__Type v__checker__Checker_promote_num(v__checker__Check
 	v__table__Type type_hi = left_type;
 	v__table__Type type_lo = right_type;
 	if (v__table__Type_idx(type_hi) < v__table__Type_idx(type_lo)) {
-		v__table__Type _var_6492 = type_hi;
-		v__table__Type _var_6501 = type_lo;
-		type_hi = _var_6501;
-		type_lo = _var_6492;
+		v__table__Type _var_6493 = type_hi;
+		v__table__Type _var_6502 = type_lo;
+		type_hi = _var_6502;
+		type_lo = _var_6493;
 	}
 	int idx_hi = v__table__Type_idx(type_hi);
 	int idx_lo = v__table__Type_idx(type_lo);
