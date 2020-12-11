66fdae2a9fe975336cc6efad6aa9a473409b4dcd
joe-conigliaro <joe@vlang.io>
2020-12-11 15:05:28 +1100

    [v:master] a297cf8 - string: fix one byte leak in clone()
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "18ec24d"
+#define V_COMMIT_HASH "a297cf8"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "c4e76e6"
+	#define V_COMMIT_HASH "18ec24d"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "18ec24d"
+	#define V_CURRENT_COMMIT_HASH "a297cf8"
 #endif
 
 // V comptime_defines:
@@ -12337,6 +12337,9 @@ VV_LOCAL_SYMBOL string string_clone_static(string a) {
 }
 
 string string_clone(string a) {
+	if ((a).len == 0) {
+		return _SLIT("");
+	}
 	string b = (string){.str = v_malloc(a.len + 1), .len = a.len};
 	{ // Unsafe block
 		memcpy(b.str, a.str, a.len);
@@ -20814,7 +20817,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("c4e76e6"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("18ec24d"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 VV_LOCAL_SYMBOL void v__pref__Preferences_try_to_use_tcc_by_default(v__pref__Preferences* p) {
