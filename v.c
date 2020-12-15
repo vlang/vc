11e99e9b3ff30e7937b4a7703796c8df776f7c8f
joe-conigliaro <joe@vlang.io>
2020-12-15 14:30:29 +1100

    [v:master] c922565 - fmt: only insert a space after // if the 3rd char is alphanumeric (#7330)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "8ab59c5"
+#define V_COMMIT_HASH "c922565"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "a3c3fd4"
+	#define V_COMMIT_HASH "8ab59c5"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "8ab59c5"
+	#define V_CURRENT_COMMIT_HASH "c922565"
 #endif
 
 // V comptime_defines:
@@ -20976,7 +20976,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("a3c3fd4"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("8ab59c5"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 VV_LOCAL_SYMBOL void v__pref__Preferences_try_to_use_tcc_by_default(v__pref__Preferences* p) {
@@ -24815,7 +24815,7 @@ VV_LOCAL_SYMBOL v__token__Token v__scanner__Scanner_text_scan(v__scanner__Scanne
 				}
 				if (v__scanner__Scanner_should_parse_comment(s)) {
 					s->line_comment = string_substr(s->text, start + 1, comment_line_end);
-					string comment = string_trim_space(s->line_comment);
+					string comment = s->line_comment;
 					bool is_separate_line_comment = true;
 					for (int j = start - 2; j >= 0 && string_at(s->text, j) != '\n'; j--) {
 						if (!(string_at(s->text, j) == '\t' || string_at(s->text, j) == ' ')) {
