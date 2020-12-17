64b695c408c40f3515709e5da30b69dcee623ec8
joe-conigliaro <joe@vlang.io>
2020-12-17 18:45:28 +1100

    [v:master] c164586 - cgen: fix map clone (#7366)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "8addb31"
+#define V_COMMIT_HASH "c164586"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "05e101b"
+	#define V_COMMIT_HASH "8addb31"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "8addb31"
+	#define V_CURRENT_COMMIT_HASH "c164586"
 #endif
 
 // V comptime_defines:
@@ -4498,7 +4498,7 @@ VV_LOCAL_SYMBOL bool map_exists(map m, string key);
 void map_delete(map* m, string key);
 array_string map_keys(map* m);
 DenseArray DenseArray_clone(DenseArray* d);
-map map_clone(map m);
+map map_clone(map* m);
 void map_free(map* m);
 VV_LOCAL_SYMBOL void opt_ok2(voidptr data, OptionBase* option, int size);
 string Option_str(Option o);
@@ -12102,20 +12102,20 @@ DenseArray DenseArray_clone(DenseArray* d) {
 }
 
 // Attr: [unsafe]
-map map_clone(map m) {
-	int metasize = ((int)(/*SizeOfType*/ sizeof(u32) * (m.cap + 2 + m.extra_metas)));
+map map_clone(map* m) {
+	int metasize = ((int)(/*SizeOfType*/ sizeof(u32) * (m->cap + 2 + m->extra_metas)));
 	map res = (map){
-		.key_bytes = m.key_bytes,
-		.value_bytes = m.value_bytes,
-		.cap = m.cap,
-		.cached_hashbits = m.cached_hashbits,
-		.shift = m.shift,
-		.key_values = DenseArray_clone(&m.key_values),
+		.key_bytes = m->key_bytes,
+		.value_bytes = m->value_bytes,
+		.cap = m->cap,
+		.cached_hashbits = m->cached_hashbits,
+		.shift = m->shift,
+		.key_values = DenseArray_clone(&m->key_values),
 		.metas = ((u32*)(v_malloc(metasize))),
-		.extra_metas = m.extra_metas,
-		.len = m.len,
+		.extra_metas = m->extra_metas,
+		.len = m->len,
 	};
-	memcpy(res.metas, m.metas, metasize);
+	memcpy(res.metas, m->metas, metasize);
 	return res;
 }
 
@@ -20950,7 +20950,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("05e101b"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("8addb31"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 VV_LOCAL_SYMBOL void v__pref__Preferences_try_to_use_tcc_by_default(v__pref__Preferences* p) {
@@ -43467,6 +43467,9 @@ VV_LOCAL_SYMBOL void v__gen__Gen_method_call(v__gen__Gen* g, v__ast__CallExpr no
 		v__gen__Gen_gen_str_for_type(g, node.receiver_type);
 	}
 	bool has_cast = false;
+	if (left_sym->kind == v__table__Kind_map && string_eq(node.name, _SLIT("clone"))) {
+		receiver_type_name = _SLIT("map");
+	}
 	if (left_sym->kind == v__table__Kind_array && (string_eq(node.name, _SLIT("repeat")) || string_eq(node.name, _SLIT("sort_with_compare")) || string_eq(node.name, _SLIT("free")) || string_eq(node.name, _SLIT("push_many")) || string_eq(node.name, _SLIT("trim")) || string_eq(node.name, _SLIT("first")) || string_eq(node.name, _SLIT("last")) || string_eq(node.name, _SLIT("pop")) || string_eq(node.name, _SLIT("clone")) || string_eq(node.name, _SLIT("reverse")) || string_eq(node.name, _SLIT("slice")))) {
 		receiver_type_name = _SLIT("array");
 		if ((string_eq(node.name, _SLIT("last")) || string_eq(node.name, _SLIT("first")) || string_eq(node.name, _SLIT("pop")))) {
@@ -43613,11 +43616,11 @@ VV_LOCAL_SYMBOL void v__gen__Gen_fn_call(v__gen__Gen* g, v__ast__CallExpr node) 
 	}
 	if (!print_auto_str) {
 		if (g->pref->is_debug && string_eq(node.name, _SLIT("panic"))) {
-			multi_return_int_string_string_string mr_18304 = v__gen__Gen_panic_debug_info(g, node.pos);
-			int paline = mr_18304.arg0;
-			string pafile = mr_18304.arg1;
-			string pamod = mr_18304.arg2;
-			string pafn = mr_18304.arg3;
+			multi_return_int_string_string_string mr_18388 = v__gen__Gen_panic_debug_info(g, node.pos);
+			int paline = mr_18388.arg0;
+			string pafile = mr_18388.arg1;
+			string pamod = mr_18388.arg2;
+			string pafn = mr_18388.arg3;
 			v__gen__Gen_write(g, _STR("panic_debug(%"PRId32"\000, tos3(\"%.*s\000\"), tos3(\"%.*s\000\"), tos3(\"%.*s\000\"),  ", 5, paline, pafile, pamod, pafn));
 			v__gen__Gen_call_args(g, node);
 			v__gen__Gen_write(g, _SLIT(")"));
