5122b6b5f8902d6f3ea7860edc66fdbbc6ba2a75
joe-conigliaro <joe@vlang.io>
2020-12-19 10:35:27 +1100

    [v:master] 1e4b3a7 - regex: code cleaning, add more tests (#7402)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "042449c"
+#define V_COMMIT_HASH "1e4b3a7"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "6854ba2"
+	#define V_COMMIT_HASH "042449c"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "042449c"
+	#define V_CURRENT_COMMIT_HASH "1e4b3a7"
 #endif
 
 // V comptime_defines:
@@ -21015,7 +21015,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("6854ba2"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("042449c"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 VV_LOCAL_SYMBOL void v__pref__Preferences_try_to_use_tcc_by_default(v__pref__Preferences* p) {
@@ -26157,6 +26157,11 @@ bool v__checker__Checker_check_basic(v__checker__Checker* c, v__table__Type got,
 	if ((v__table__TypeSymbol_is_int(got_type_sym) && exp_type_sym->kind == v__table__Kind_enum_) || (v__table__TypeSymbol_is_int(exp_type_sym) && got_type_sym->kind == v__table__Kind_enum_)) {
 		return true;
 	}
+	if (got_type_sym->kind == v__table__Kind_array && exp_type_sym->kind == v__table__Kind_array) {
+		if (string_eq(v__table__Table_type_to_str(c->table, got), v__table__Table_type_to_str(c->table, expected))) {
+			return true;
+		}
+	}
 	if (got_type_sym->kind == v__table__Kind_array_fixed && exp_type_sym->kind == v__table__Kind_byteptr) {
 		v__table__ArrayFixed info = /* as */ *(v__table__ArrayFixed*)__as_cast((got_type_sym->info)._v__table__ArrayFixed, (got_type_sym->info).typ, /*expected:*/352);
 		if (v__table__Type_idx(info.elem_type) == _const_v__table__byte_type_idx) {
@@ -26262,10 +26267,10 @@ VV_LOCAL_SYMBOL v__table__Type v__checker__Checker_promote_num(v__checker__Check
 	v__table__Type type_hi = left_type;
 	v__table__Type type_lo = right_type;
 	if (v__table__Type_idx(type_hi) < v__table__Type_idx(type_lo)) {
-		v__table__Type _var_6567 = type_hi;
-		v__table__Type _var_6576 = type_lo;
-		type_hi = _var_6576;
-		type_lo = _var_6567;
+		v__table__Type _var_6633 = type_hi;
+		v__table__Type _var_6642 = type_lo;
+		type_hi = _var_6642;
+		type_lo = _var_6633;
 	}
 	int idx_hi = v__table__Type_idx(type_hi);
 	int idx_lo = v__table__Type_idx(type_lo);
