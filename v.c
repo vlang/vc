9c05bf8df31cdddadc9a3d22874aa4da2ac08c71
joe-conigliaro <joe@vlang.io>
2020-12-20 14:55:31 +1100

    [v:master] 8293348 - table: minor optimization of types.v (#7419)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "8278af4"
+#define V_COMMIT_HASH "8293348"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "cd30dcc"
+	#define V_COMMIT_HASH "8278af4"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "8278af4"
+	#define V_CURRENT_COMMIT_HASH "8293348"
 #endif
 
 // V comptime_defines:
@@ -21190,7 +21190,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("cd30dcc"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("8278af4"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 VV_LOCAL_SYMBOL void v__pref__Preferences_try_to_use_tcc_by_default(v__pref__Preferences* p) {
@@ -23903,7 +23903,12 @@ v__table__Kind v__table__Table_type_kind(v__table__Table* table, v__table__Type 
 }
 
 string v__table__TypeSymbol_str(v__table__TypeSymbol* t) {
-	return string_replace(t->name, _SLIT("array_"), _SLIT("[]"));
+	if ((t->kind == v__table__Kind_array || t->kind == v__table__Kind_array_fixed)) {
+		return string_replace(t->name, _SLIT("array_"), _SLIT("[]"));
+	} else {
+		return t->name;
+	}
+	return (string){.str=(byteptr)""};
 }
 
 // Attr: [inline]
