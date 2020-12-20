e8881979983818c22d41f056c7187fcb94787050
joe-conigliaro <joe@vlang.io>
2020-12-20 22:25:39 +1100

    [v:master] 628021a - cgen: v fmt fn.v
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "3b20838"
+#define V_COMMIT_HASH "628021a"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "4b45f2e"
+	#define V_COMMIT_HASH "3b20838"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "3b20838"
+	#define V_CURRENT_COMMIT_HASH "628021a"
 #endif
 
 // V comptime_defines:
@@ -21191,7 +21191,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("4b45f2e"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("3b20838"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 VV_LOCAL_SYMBOL void v__pref__Preferences_try_to_use_tcc_by_default(v__pref__Preferences* p) {
@@ -44051,11 +44051,11 @@ VV_LOCAL_SYMBOL void v__gen__Gen_fn_call(v__gen__Gen* g, v__ast__CallExpr node) 
 	}
 	if (!print_auto_str) {
 		if (g->pref->is_debug && string_eq(node.name, _SLIT("panic"))) {
-			multi_return_int_string_string_string mr_18428 = v__gen__Gen_panic_debug_info(g, node.pos);
-			int paline = mr_18428.arg0;
-			string pafile = mr_18428.arg1;
-			string pamod = mr_18428.arg2;
-			string pafn = mr_18428.arg3;
+			multi_return_int_string_string_string mr_18430 = v__gen__Gen_panic_debug_info(g, node.pos);
+			int paline = mr_18430.arg0;
+			string pafile = mr_18430.arg1;
+			string pamod = mr_18430.arg2;
+			string pafn = mr_18430.arg3;
 			v__gen__Gen_write(g, _STR("panic_debug(%"PRId32"\000, tos3(\"%.*s\000\"), tos3(\"%.*s\000\"), tos3(\"%.*s\000\"),  ", 5, paline, pafile, pamod, pafn));
 			v__gen__Gen_call_args(g, node);
 			v__gen__Gen_write(g, _SLIT(")"));
