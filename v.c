a8a6c3c81e98975bf96dfa11ad711ce37be4c24e
joe-conigliaro <joe@vlang.io>
2020-12-20 22:05:28 +1100

    [v:master] 3b20838 - cgen: fix module builds when -usecache is on by default
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "4b45f2e"
+#define V_COMMIT_HASH "3b20838"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "b1f6ff8"
+	#define V_COMMIT_HASH "4b45f2e"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "4b45f2e"
+	#define V_CURRENT_COMMIT_HASH "3b20838"
 #endif
 
 // V comptime_defines:
@@ -21191,7 +21191,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("b1f6ff8"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("4b45f2e"), _STR("%.*s\000 | %.*s\000 | %.*s", 3, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 VV_LOCAL_SYMBOL void v__pref__Preferences_try_to_use_tcc_by_default(v__pref__Preferences* p) {
@@ -38276,7 +38276,7 @@ VV_LOCAL_SYMBOL void v__gen__Gen_stmt(v__gen__Gen* g, v__ast__Stmt node) {
 				println(_STR("build module `%.*s\000` fn `%.*s\000`", 3, g->module_built, (*node._v__ast__FnDecl).name));
 			}
 		}
-		if (g->pref->use_cache) {
+		if (g->pref->use_cache && g->pref->build_mode != v__pref__BuildMode_build_module) {
 			if (string_ne((*node._v__ast__FnDecl).mod, _SLIT("main")) && string_ne((*node._v__ast__FnDecl).mod, _SLIT("help")) && !should_bundle_module) {
 				skip = true;
 			}
@@ -41778,9 +41778,9 @@ array_v__table__Field _t1532_orig = (*typ.info._v__table__Struct).fields;
 					if (v__table__Type_has_flag(field.typ, v__table__TypeFlag_optional)) {
 						string last_text = string_clone(strings__Builder_after(&g->type_definitions, start_pos));
 						strings__Builder_go_back_to(&g->type_definitions, start_pos);
-						multi_return_string_string mr_139634 = v__gen__Gen_optional_type_name(g, field.typ);
-						string styp = mr_139634.arg0;
-						string base = mr_139634.arg1;
+						multi_return_string_string mr_139672 = v__gen__Gen_optional_type_name(g, field.typ);
+						string styp = mr_139672.arg0;
+						string base = mr_139672.arg1;
 						array_push(&g->optionals, _MOV((string[]){ string_clone(styp) }));
 						strings__Builder_writeln(&g->typedefs2, _STR("typedef struct %.*s\000 %.*s\000;", 3, styp, styp));
 						strings__Builder_writeln(&g->type_definitions, _STR("%.*s\000;", 2, v__gen__Gen_optional_type_text(g, styp, base)));
@@ -42216,11 +42216,11 @@ VV_LOCAL_SYMBOL void v__gen__Gen_or_block(v__gen__Gen* g, string var_name, v__as
 	} else if (or_block.kind == v__ast__OrKind_propagate) {
 		if (string_eq(g->file.mod.name, _SLIT("main")) && (isnil(g->fn_decl) || string_eq(g->fn_decl->name, _SLIT("main.main")))) {
 			if (g->pref->is_debug) {
-				multi_return_int_string_string_string mr_153669 = v__gen__Gen_panic_debug_info(g, or_block.pos);
-				int paline = mr_153669.arg0;
-				string pafile = mr_153669.arg1;
-				string pamod = mr_153669.arg2;
-				string pafn = mr_153669.arg3;
+				multi_return_int_string_string_string mr_153707 = v__gen__Gen_panic_debug_info(g, or_block.pos);
+				int paline = mr_153707.arg0;
+				string pafile = mr_153707.arg1;
+				string pamod = mr_153707.arg2;
+				string pafn = mr_153707.arg3;
 				v__gen__Gen_writeln(g, _STR("panic_debug(%"PRId32"\000, tos3(\"%.*s\000\"), tos3(\"%.*s\000\"), tos3(\"%.*s\000\"), %.*s\000.v_error );", 6, paline, pafile, pamod, pafn, cvar_name));
 			} else {
 				v__gen__Gen_writeln(g, _STR("\tv_panic(_STR(\"optional not set (%%.*s\\000)\", 2, %.*s\000.v_error));", 2, cvar_name));
@@ -43653,7 +43653,7 @@ VV_LOCAL_SYMBOL void v__gen__Gen_gen_fn_decl(v__gen__Gen* g, v__ast__FnDecl it, 
 	array_string fargs = mr_3765.arg0;
 	array_string fargtypes = mr_3765.arg1;
 	string arg_str = strings__Builder_after(&g->out, arg_start_pos);
-	if (it.no_body || (g->pref->use_cache && it.is_builtin) || skip) {
+	if (it.no_body || ((g->pref->use_cache && g->pref->build_mode != v__pref__BuildMode_build_module) && it.is_builtin) || skip) {
 		strings__Builder_writeln(&g->definitions, _SLIT(");"));
 		v__gen__Gen_writeln(g, _SLIT(");"));
 		return;
@@ -44051,11 +44051,11 @@ VV_LOCAL_SYMBOL void v__gen__Gen_fn_call(v__gen__Gen* g, v__ast__CallExpr node) 
 	}
 	if (!print_auto_str) {
 		if (g->pref->is_debug && string_eq(node.name, _SLIT("panic"))) {
-			multi_return_int_string_string_string mr_18388 = v__gen__Gen_panic_debug_info(g, node.pos);
-			int paline = mr_18388.arg0;
-			string pafile = mr_18388.arg1;
-			string pamod = mr_18388.arg2;
-			string pafn = mr_18388.arg3;
+			multi_return_int_string_string_string mr_18428 = v__gen__Gen_panic_debug_info(g, node.pos);
+			int paline = mr_18428.arg0;
+			string pafile = mr_18428.arg1;
+			string pamod = mr_18428.arg2;
+			string pafn = mr_18428.arg3;
 			v__gen__Gen_write(g, _STR("panic_debug(%"PRId32"\000, tos3(\"%.*s\000\"), tos3(\"%.*s\000\"), tos3(\"%.*s\000\"),  ", 5, paline, pafile, pamod, pafn));
 			v__gen__Gen_call_args(g, node);
 			v__gen__Gen_write(g, _SLIT(")"));
