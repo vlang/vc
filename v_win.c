d37e2abb3784bbba4b3aa423976e23c27af1efc0
joe-conigliaro <joe@vlang.io>
2020-12-28 05:10:28 +1100

    [v:master] d87011a - cgen: fix generic contains method (#7618)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "b9df7aa"
+#define V_COMMIT_HASH "d87011a"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "df61cf2"
+	#define V_COMMIT_HASH "b9df7aa"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "b9df7aa"
+	#define V_CURRENT_COMMIT_HASH "d87011a"
 #endif
 
 // V comptime_defines:
@@ -21196,7 +21196,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("df61cf2"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("b9df7aa"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 VV_LOCAL_SYMBOL void v__pref__Preferences_find_cc_if_cross_compiling(v__pref__Preferences* p) {
@@ -37624,7 +37624,7 @@ VV_LOCAL_SYMBOL string v__gen__Gen_gen_struct_equality_fn(v__gen__Gen* g, v__tab
 			strings__Builder_writeln(&fn_builder, _STR("\tif (*((voidptr*)(a.%.*s\000)) != *((voidptr*)(b.%.*s\000))) {", 3, field.name, field.name));
 		}
 		else {
-			strings__Builder_writeln(&fn_builder, _STR("\tif (a.%.*s\000 != b.%.*s\000)) {", 3, field.name, field.name));
+			strings__Builder_writeln(&fn_builder, _STR("\tif (a.%.*s\000 != b.%.*s\000) {", 3, field.name, field.name));
 		};
 		strings__Builder_writeln(&fn_builder, _SLIT("\t\treturn false;"));
 		strings__Builder_writeln(&fn_builder, _SLIT("\t}"));
