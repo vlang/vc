b1cf5c1f639ababcfea4a0f14819797343218b4e
joe-conigliaro <joe@vlang.io>
2020-12-27 23:45:29 +1100

    [v:master] 929a002 - vdoc: move to it's own directory cmd/tools/vdoc/ (#7606)
    on-behalf-of: @vlang <joe@vlang.io>

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "c9081a8"
+#define V_COMMIT_HASH "929a002"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "0210d1b"
+	#define V_COMMIT_HASH "c9081a8"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "c9081a8"
+	#define V_CURRENT_COMMIT_HASH "929a002"
 #endif
 
 // V comptime_defines:
@@ -21255,7 +21255,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("0210d1b"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("c9081a8"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 }
 
 VV_LOCAL_SYMBOL void v__pref__Preferences_find_cc_if_cross_compiling(v__pref__Preferences* p) {
@@ -22660,8 +22660,8 @@ void v__util__launch_tool(bool is_verbose, string tool_name, array_string args) 
 	v__util__set_vroot_folder(vroot);
 	string tool_args = v__util__args_quote_paths(args);
 	string tool_basename = os__real_path(os__join_path(vroot, (varg_string){.len=3,.args={_SLIT("cmd"), _SLIT("tools"), tool_name}}));
-	string tool_exe = v__util__path_of_executable(tool_basename);
-	string tool_source = string_add(tool_basename, _SLIT(".v"));
+	string tool_exe = (os__is_dir(tool_basename) ? (v__util__path_of_executable(os__join_path(tool_basename, (varg_string){.len=1,.args={tool_name}}))) : (v__util__path_of_executable(tool_basename)));
+	string tool_source = (os__is_dir(tool_basename) ? (tool_basename) : (string_add(tool_basename, _SLIT(".v"))));
 	string tool_command = _STR("\"%.*s\000\" %.*s", 2, tool_exe, tool_args);
 	if (is_verbose) {
 		println(_STR("launch_tool vexe        : %.*s", 1, vroot));
