6fc182f1075acb2931c4ec5c9089255ab05806f3
joe-conigliaro <joe@vlang.io>
2020-12-29 16:50:29 +1100

    [v:master] b87283e - cgen: minor optimization of auto_eq_methods.v (#7668)
    on-behalf-of: @vlang <joe@vlang.io>

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "954580f"
+#define V_COMMIT_HASH "b87283e"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "fe7fa70"
+	#define V_COMMIT_HASH "954580f"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "954580f"
+	#define V_CURRENT_COMMIT_HASH "b87283e"
 #endif
 
 // V comptime_defines:
@@ -21138,7 +21138,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("fe7fa70"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("954580f"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 	if (string_eq(os__user_os(), _SLIT("windows"))) {
 		p->use_cache = false;
 	}
@@ -37551,13 +37551,13 @@ VV_LOCAL_SYMBOL void v__gen__Gen_gen_array_index(v__gen__Gen* g, v__ast__CallExp
 }
 
 VV_LOCAL_SYMBOL string v__gen__Gen_gen_struct_equality_fn(v__gen__Gen* g, v__table__Type left) {
-	v__table__TypeSymbol* left_sym = v__table__Table_get_type_symbol(g->table, left);
-	v__table__Struct info = v__table__TypeSymbol_struct_info(left_sym);
 	string ptr_typ = string_trim(v__gen__Gen_typ(g, left), _SLIT("*"));
 	if ((array_string_contains(g->struct_fn_definitions, ptr_typ))) {
 		return ptr_typ;
 	}
 	array_push(&g->struct_fn_definitions, _MOV((string[]){ string_clone(ptr_typ) }));
+	v__table__TypeSymbol* left_sym = v__table__Table_get_type_symbol(g->table, left);
+	v__table__Struct info = v__table__TypeSymbol_struct_info(left_sym);
 	strings__Builder_writeln(&g->type_definitions, _STR("static bool %.*s\000_struct_eq(%.*s\000 a, %.*s\000 b); // auto", 4, ptr_typ, ptr_typ, ptr_typ));
 	strings__Builder fn_builder = strings__new_builder(512);
 	strings__Builder_writeln(&fn_builder, _STR("static bool %.*s\000_struct_eq(%.*s\000 a, %.*s\000 b) {", 4, ptr_typ, ptr_typ, ptr_typ));
@@ -37592,15 +37592,15 @@ VV_LOCAL_SYMBOL string v__gen__Gen_gen_struct_equality_fn(v__gen__Gen* g, v__tab
 }
 
 VV_LOCAL_SYMBOL string v__gen__Gen_gen_array_equality_fn(v__gen__Gen* g, v__table__Type left) {
-	v__table__TypeSymbol* left_sym = v__table__Table_get_type_symbol(g->table, left);
 	string ptr_typ = string_trim(v__gen__Gen_typ(g, left), _SLIT("*"));
-	v__table__Type elem_typ = v__table__TypeSymbol_array_info(left_sym).elem_type;
-	string ptr_elem_typ = v__gen__Gen_typ(g, elem_typ);
-	v__table__TypeSymbol* elem_sym = v__table__Table_get_type_symbol(g->table, elem_typ);
 	if ((array_string_contains(g->array_fn_definitions, ptr_typ))) {
 		return ptr_typ;
 	}
 	array_push(&g->array_fn_definitions, _MOV((string[]){ string_clone(ptr_typ) }));
+	v__table__TypeSymbol* left_sym = v__table__Table_get_type_symbol(g->table, left);
+	v__table__Type elem_typ = v__table__TypeSymbol_array_info(left_sym).elem_type;
+	string ptr_elem_typ = v__gen__Gen_typ(g, elem_typ);
+	v__table__TypeSymbol* elem_sym = v__table__Table_get_type_symbol(g->table, elem_typ);
 	strings__Builder_writeln(&g->type_definitions, _STR("static bool %.*s\000_arr_eq(%.*s\000 a, %.*s\000 b); // auto", 4, ptr_typ, ptr_typ, ptr_typ));
 	strings__Builder fn_builder = strings__new_builder(512);
 	strings__Builder_writeln(&fn_builder, _STR("static bool %.*s\000_arr_eq(%.*s\000 a, %.*s\000 b) {", 4, ptr_typ, ptr_typ, ptr_typ));
@@ -37634,15 +37634,15 @@ VV_LOCAL_SYMBOL string v__gen__Gen_gen_array_equality_fn(v__gen__Gen* g, v__tabl
 }
 
 VV_LOCAL_SYMBOL string v__gen__Gen_gen_map_equality_fn(v__gen__Gen* g, v__table__Type left) {
-	v__table__TypeSymbol* left_sym = v__table__Table_get_type_symbol(g->table, left);
 	string ptr_typ = string_trim(v__gen__Gen_typ(g, left), _SLIT("*"));
 	if ((array_string_contains(g->map_fn_definitions, ptr_typ))) {
 		return ptr_typ;
 	}
+	array_push(&g->map_fn_definitions, _MOV((string[]){ string_clone(ptr_typ) }));
+	v__table__TypeSymbol* left_sym = v__table__Table_get_type_symbol(g->table, left);
 	v__table__Type value_typ = v__table__TypeSymbol_map_info(left_sym).value_type;
 	v__table__TypeSymbol* value_sym = v__table__Table_get_type_symbol(g->table, value_typ);
 	string ptr_value_typ = v__gen__Gen_typ(g, value_typ);
-	array_push(&g->map_fn_definitions, _MOV((string[]){ string_clone(ptr_typ) }));
 	strings__Builder_writeln(&g->type_definitions, _STR("static bool %.*s\000_map_eq(%.*s\000 a, %.*s\000 b); // auto", 4, ptr_typ, ptr_typ, ptr_typ));
 	strings__Builder fn_builder = strings__new_builder(512);
 	strings__Builder_writeln(&fn_builder, _STR("static bool %.*s\000_map_eq(%.*s\000 a, %.*s\000 b) {", 4, ptr_typ, ptr_typ, ptr_typ));
