ee55c963d739dc9ccffdad2af374d84ffe81e7c1
joe-conigliaro <joe@vlang.io>
2021-01-14 21:35:32 +1100

    [v:master] 9003ea7 - builtin: fix a warning for `-d debug_realloc`

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "ca5f88e"
+#define V_COMMIT_HASH "9003ea7"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "aa87816"
+	#define V_COMMIT_HASH "ca5f88e"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "ca5f88e"
+	#define V_CURRENT_COMMIT_HASH "9003ea7"
 #endif
 
 // V comptime_defines:
@@ -11228,10 +11228,12 @@ byteptr v_realloc(byteptr b, int n) {
 	{
 		#if defined(CUSTOM_DEFINE_debug_realloc)
 		{
-			new_ptr = v_malloc(n);
-			memcpy(new_ptr, b, n);
-			memset(b, 0x57, n);
-			free(b);
+			{ // Unsafe block
+				new_ptr = v_malloc(n);
+				memcpy(new_ptr, b, n);
+				memset(b, 0x57, n);
+				free(b);
+			}
 		}
 		#else
 		{
@@ -22061,7 +22063,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("aa87816"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("ca5f88e"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 	if (string_eq(os__user_os(), _SLIT("windows"))) {
 		p->use_cache = false;
 	}
