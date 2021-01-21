6437bc659dbf8273bf9a71d638689ed48ae8734e
joe-conigliaro <joe@vlang.io>
2021-01-21 20:45:33 +1100

    [v:master] d975436 - pkgconfig: add support for Fedora and CentOS Linux. (#8237)

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "242c576"
+#define V_COMMIT_HASH "d975436"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "f059a9e"
+	#define V_COMMIT_HASH "242c576"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "242c576"
+	#define V_CURRENT_COMMIT_HASH "d975436"
 #endif
 
 // V comptime_defines:
@@ -21972,7 +21972,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("f059a9e"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("242c576"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 	if (string_eq(os__user_os(), _SLIT("windows"))) {
 		p->use_cache = false;
 	}
@@ -54047,7 +54047,7 @@ void _vinit(int ___argc, voidptr ___argv) {
 	_const_rand__util__u63_mask = ((u64)(0x7FFFFFFFFFFFFFFFU));
 	// Initializations for module v.errors :
 	// Initializations for module v.pkgconfig :
-	_const_v__pkgconfig__default_paths = new_array_from_c_array(6, 6, sizeof(string), _MOV((string[6]){_SLIT("/usr/local/lib/x86_64-linux-gnu/pkgconfig"), _SLIT("/usr/local/lib/pkgconfig"), _SLIT("/usr/local/share/pkgconfig"), _SLIT("/usr/lib/x86_64-linux-gnu/pkgconfig"), _SLIT("/usr/lib/pkgconfig"), _SLIT("/usr/share/pkgconfig")}));
+	_const_v__pkgconfig__default_paths = new_array_from_c_array(8, 8, sizeof(string), _MOV((string[8]){_SLIT("/usr/local/lib/x86_64-linux-gnu/pkgconfig"), _SLIT("/usr/local/lib64/pkgconfig"), _SLIT("/usr/local/lib/pkgconfig"), _SLIT("/usr/local/share/pkgconfig"), _SLIT("/usr/lib/x86_64-linux-gnu/pkgconfig"), _SLIT("/usr/lib64/pkgconfig"), _SLIT("/usr/lib/pkgconfig"), _SLIT("/usr/share/pkgconfig")}));
 	// Initializations for module vweb.tmpl :
 	// Initializations for module v.vet :
 	// Initializations for module runtime :
