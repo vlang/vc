311ea4bc68a7511ef94b7710ff379bc5c2ecba5e
joe-conigliaro <joe@vlang.io>
2021-01-22 06:20:32 +1100

    [v:master] 0d20460 - gen: fix generic comp time if (#8254)

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "d1286db"
+#define V_COMMIT_HASH "0d20460"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "6c24e9b"
+	#define V_COMMIT_HASH "d1286db"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "d1286db"
+	#define V_CURRENT_COMMIT_HASH "0d20460"
 #endif
 
 // V comptime_defines:
@@ -21973,7 +21973,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("6c24e9b"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("d1286db"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 	if (string_eq(os__user_os(), _SLIT("windows"))) {
 		p->use_cache = false;
 	}
@@ -46899,6 +46899,11 @@ VV_LOCAL_SYMBOL void v__gen__Gen_comp_if(v__gen__Gen* g, v__ast__IfExpr node) {
 			}
 			if (should_create_scope) {
 				v__gen__Gen_writeln(g, _SLIT("}"));
+			}
+			if (!comp_if_stmts_skip && (branch.cond).typ == 237 /* v.ast.InfixExpr */) {
+				if ((/* as */ *(v__ast__InfixExpr*)__as_cast((branch.cond)._v__ast__InfixExpr, (branch.cond).typ, /*expected:*/237)).op == v__token__Kind_key_is) {
+					break;
+				}
 			}
 		}
 		g->defer_ifdef = _SLIT("");
