e04bdfb8459f061f285822b16fd36585178efd5b
joe-conigliaro <joe@vlang.io>
2021-02-02 21:25:31 +1100

    [v:master] 17062dc - v.pref: support `-obf` in addition to `-obfuscate`, as described in `v help build`

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "5aaeac7"
+#define V_COMMIT_HASH "17062dc"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "d57a9c4"
+	#define V_COMMIT_HASH "5aaeac7"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "5aaeac7"
+	#define V_CURRENT_COMMIT_HASH "17062dc"
 #endif
 
 // V comptime_defines:
@@ -22491,7 +22491,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("d57a9c4"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("5aaeac7"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, array_string_str(p->compile_defines_all)), _STR("%.*s", 1, array_string_str(p->compile_defines)), _STR("%.*s", 1, array_string_str(p->lookup_path))})));
 	if (string_eq(os__user_os(), _SLIT("windows"))) {
 		p->use_cache = false;
 	}
@@ -22858,7 +22858,7 @@ if (string_eq(		arg, _SLIT("-apk"))) {
 		else if (string_eq(		arg, _SLIT("-stats"))) {
 			res->is_stats = true;
 		}
-		else if (string_eq(		arg, _SLIT("-obfuscate"))) {
+		else if (string_eq(		arg, _SLIT("-obf")) || string_eq(		arg, _SLIT("-obfuscate"))) {
 			res->obfuscate = true;
 		}
 		else if (string_eq(		arg, _SLIT("-translated"))) {
