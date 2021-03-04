14794671d0289777e8966a94554de5b16e0180b5
joe-conigliaro <joe@vlang.io>
2021-03-04 23:40:21 +1100

    [v:master] c65d65a - v: use dash instead of lowerdash for -print-v-files (#9111)

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "eb1e1af"
+#define V_COMMIT_HASH "c65d65a"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "7c89389"
+	#define V_COMMIT_HASH "eb1e1af"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "eb1e1af"
+	#define V_CURRENT_COMMIT_HASH "c65d65a"
 #endif
 
 // V comptime_defines:
@@ -22930,7 +22930,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("7c89389"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, Array_string_str(p->compile_defines_all)), _STR("%.*s", 1, Array_string_str(p->compile_defines)), _STR("%.*s", 1, Array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("eb1e1af"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, Array_string_str(p->compile_defines_all)), _STR("%.*s", 1, Array_string_str(p->compile_defines)), _STR("%.*s", 1, Array_string_str(p->lookup_path))})));
 	if (string_eq(os__user_os(), _SLIT("windows"))) {
 		p->use_cache = false;
 	}
@@ -23397,7 +23397,7 @@ multi_return_v__pref__Preferences_string v__pref__parse_args(Array_string known_
 		else if (string_eq(arg, _SLIT("-w"))) {
 			res->skip_warnings = true;
 		}
-		else if (string_eq(arg, _SLIT("-print_v_files"))) {
+		else if (string_eq(arg, _SLIT("-print-v-files"))) {
 			res->print_v_files = true;
 		}
 		else if (string_eq(arg, _SLIT("-error-limit"))) {
