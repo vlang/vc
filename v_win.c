d8377b1396fdef747ca02b01858074f662a9d1c8
joe-conigliaro <joe@vlang.io>
2021-03-07 05:40:21 +1100

    [v:master] 7f7f9dc - parser,fmt: fix regression with non-void arrays in if conditions (#9161)

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "9917c64"
+#define V_COMMIT_HASH "7f7f9dc"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "849cde2"
+	#define V_COMMIT_HASH "9917c64"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "9917c64"
+	#define V_CURRENT_COMMIT_HASH "7f7f9dc"
 #endif
 
 // V comptime_defines:
@@ -22764,7 +22764,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("849cde2"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, Array_string_str(p->compile_defines_all)), _STR("%.*s", 1, Array_string_str(p->compile_defines)), _STR("%.*s", 1, Array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("9917c64"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, Array_string_str(p->compile_defines_all)), _STR("%.*s", 1, Array_string_str(p->compile_defines)), _STR("%.*s", 1, Array_string_str(p->lookup_path))})));
 	if (string_eq(os__user_os(), _SLIT("windows"))) {
 		p->use_cache = false;
 	}
@@ -37860,7 +37860,7 @@ VV_LOCAL_SYMBOL v__ast__ArrayInit v__parser__Parser_array_init(v__parser__Parser
 	bool has_cap = false;
 	v__ast__Expr len_expr = (v__ast__Expr){0};
 	v__ast__Expr cap_expr = (v__ast__Expr){0};
-	if (p->tok.kind == v__token__Kind_lcbr && exprs.len == 0 && !p->inside_if) {
+	if (p->tok.kind == v__token__Kind_lcbr && exprs.len == 0 && !v__table__Type_alias_eq(array_type, _const_v__table__void_type)) {
 		v__parser__Parser_next(p);
 		for (;;) {
 			if (!(p->tok.kind != v__token__Kind_rcbr)) break;
