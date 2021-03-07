a8d697f7cb08d620f05c432051c1d6303b389641
joe-conigliaro <joe@vlang.io>
2021-03-07 18:45:20 +1100

    [v:master] 6048d0e - fmt: fix backslash doubling in attribute string args (#9175)

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "053d687"
+#define V_COMMIT_HASH "6048d0e"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "fdcfe39"
+	#define V_COMMIT_HASH "053d687"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "053d687"
+	#define V_CURRENT_COMMIT_HASH "6048d0e"
 #endif
 
 // V comptime_defines:
@@ -22766,7 +22766,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("fdcfe39"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, Array_string_str(p->compile_defines_all)), _STR("%.*s", 1, Array_string_str(p->compile_defines)), _STR("%.*s", 1, Array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("053d687"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, Array_string_str(p->compile_defines_all)), _STR("%.*s", 1, Array_string_str(p->compile_defines)), _STR("%.*s", 1, Array_string_str(p->lookup_path))})));
 	if (string_eq(os__user_os(), _SLIT("windows"))) {
 		p->use_cache = false;
 	}
@@ -25020,8 +25020,7 @@ string v__table__Attr_str(v__table__Attr attr) {
 		if (attr.arg.len > 0) {
 			s = /*f*/string_add(s, _SLIT(": "));
 			if (attr.is_string_arg) {
-				string a = string_replace(attr.arg, _SLIT("\\"), _SLIT("\\\\"));
-				a = string_replace(a, _SLIT("'"), _SLIT("\\'"));
+				string a = string_replace(attr.arg, _SLIT("'"), _SLIT("\\'"));
 				s = /*f*/string_add(s, _STR("'%.*s\000'", 2, a));
 			} else {
 				s = /*f*/string_add(s, attr.arg);
