2814e9f1bacd0164dcc7bbb39207b4c5698a5e93
joe-conigliaro <joe@vlang.io>
2021-03-10 16:35:21 +1100

    [v:master] fd2845e - v.util: change naming behaviour for modules relative to working dir. closes #9138

--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "96a9e16"
+#define V_COMMIT_HASH "fd2845e"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "26cfd0e"
+	#define V_COMMIT_HASH "96a9e16"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "96a9e16"
+	#define V_CURRENT_COMMIT_HASH "fd2845e"
 #endif
 
 // V comptime_defines:
@@ -23325,7 +23325,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("26cfd0e"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, Array_string_str(p->compile_defines_all)), _STR("%.*s", 1, Array_string_str(p->compile_defines)), _STR("%.*s", 1, Array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("96a9e16"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, Array_string_str(p->compile_defines_all)), _STR("%.*s", 1, Array_string_str(p->compile_defines)), _STR("%.*s", 1, Array_string_str(p->lookup_path))})));
 	if (string_eq(os__user_os(), _SLIT("windows"))) {
 		p->use_cache = false;
 	}
@@ -24544,6 +24544,10 @@ string v__util__qualify_module(string mod, string file_path) {
 		;
 	}
 	string clean_file_path = string_all_before_last(file_path, _SLIT("/"));
+	if (string_eq(string_replace(clean_file_path, string_add(os__getwd(), _const_os__path_separator), _SLIT("")), mod)) {
+		return mod;
+		;
+	}
 	;
 	{ /* if guard */ 
 	Option2_string _t1140;
