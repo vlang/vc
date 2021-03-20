d072e1666c4841fb56fb7e3c2df865d085800f0d
joe-conigliaro <joe@vlang.io>
2021-03-21 01:30:21 +1100

    [v:master] 517c184 - time: fix .sleep() with `-gc boehm`

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "8efea1e"
+#define V_COMMIT_HASH "517c184"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "a6ddd24"
+	#define V_COMMIT_HASH "8efea1e"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "8efea1e"
+	#define V_CURRENT_COMMIT_HASH "517c184"
 #endif
 
 // V comptime_defines:
@@ -906,6 +906,8 @@ static voidptr memfreedup(voidptr ptr, voidptr src, int sz) {
 #include <time.h>
 #endif
 
+// added by module `time`:
+#include <errno.h>
 // added by module `term`:
 
 #if defined(__has_include)
@@ -19929,8 +19931,16 @@ void time__wait(time__Duration duration) {
 }
 
 void time__sleep(time__Duration duration) {
-	struct timespec* ts = (struct timespec*)memdup(&(struct timespec){.tv_sec = duration / _const_time__second,.tv_nsec = duration % _const_time__second,}, sizeof(struct timespec));
-	nanosleep(ts, NULL);
+	struct timespec req = (struct timespec){.tv_sec = duration / _const_time__second,.tv_nsec = duration % _const_time__second,};
+	struct timespec rem = (struct timespec){.tv_sec = 0,.tv_nsec = 0,};
+	for (;;) {
+		if (!(nanosleep(&req, &rem) < 0)) break;
+		if (errno == EINTR) {
+			req = rem;
+		} else {
+			break;
+		}
+	}
 }
 
 time__Time time__unix(int abs) {
@@ -23729,7 +23739,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("a6ddd24"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, Array_string_str(p->compile_defines_all)), _STR("%.*s", 1, Array_string_str(p->compile_defines)), _STR("%.*s", 1, Array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("8efea1e"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, Array_string_str(p->compile_defines_all)), _STR("%.*s", 1, Array_string_str(p->compile_defines)), _STR("%.*s", 1, Array_string_str(p->lookup_path))})));
 	if (string_eq(os__user_os(), _SLIT("windows"))) {
 		p->use_cache = false;
 	}
--- v_win.c
+++ v_win.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "8efea1e"
+#define V_COMMIT_HASH "517c184"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "a6ddd24"
+	#define V_COMMIT_HASH "8efea1e"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "8efea1e"
+	#define V_CURRENT_COMMIT_HASH "517c184"
 #endif
 
 // V comptime_defines:
@@ -23397,7 +23397,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("a6ddd24"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, Array_string_str(p->compile_defines_all)), _STR("%.*s", 1, Array_string_str(p->compile_defines)), _STR("%.*s", 1, Array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("8efea1e"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, Array_string_str(p->compile_defines_all)), _STR("%.*s", 1, Array_string_str(p->compile_defines)), _STR("%.*s", 1, Array_string_str(p->lookup_path))})));
 	if (string_eq(os__user_os(), _SLIT("windows"))) {
 		p->use_cache = false;
 	}
