b91caf54cb537bb30dea36291387ce77239a1dd8
joe-conigliaro <joe@vlang.io>
2021-03-23 18:55:21 +1100

    [v:master] b9cbb4f - scanner: fix eof token position (#9432)

--- v.c
+++ v.c
@@ -1,11 +1,11 @@
-#define V_COMMIT_HASH "aa4e22c"
+#define V_COMMIT_HASH "b9cbb4f"
 
 #ifndef V_COMMIT_HASH
-	#define V_COMMIT_HASH "3753a58"
+	#define V_COMMIT_HASH "aa4e22c"
 #endif
 
 #ifndef V_CURRENT_COMMIT_HASH
-	#define V_CURRENT_COMMIT_HASH "aa4e22c"
+	#define V_CURRENT_COMMIT_HASH "b9cbb4f"
 #endif
 
 // V comptime_defines:
@@ -23805,7 +23805,7 @@ void v__pref__Preferences_fill_with_defaults(v__pref__Preferences* p) {
 		}
 		#endif
 	}
-	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("3753a58"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, Array_string_str(p->compile_defines_all)), _STR("%.*s", 1, Array_string_str(p->compile_defines)), _STR("%.*s", 1, Array_string_str(p->lookup_path))})));
+	p->cache_manager = v__vcache__new_cache_manager(new_array_from_c_array(7, 7, sizeof(string), _MOV((string[7]){_SLIT("aa4e22c"), _STR("%.*s\000 | %.*s\000 | %.*s\000 | %.*s\000 | %.*s", 5, v__pref__Backend_str(p->backend), v__pref__OS_str(p->os), p->ccompiler, p->is_prod ? _SLIT("true") : _SLIT("false"), p->sanitize ? _SLIT("true") : _SLIT("false")), string_trim_space(p->cflags), string_trim_space(p->third_party_option), _STR("%.*s", 1, Array_string_str(p->compile_defines_all)), _STR("%.*s", 1, Array_string_str(p->compile_defines)), _STR("%.*s", 1, Array_string_str(p->lookup_path))})));
 	if (string_eq(os__user_os(), _SLIT("windows"))) {
 		p->use_cache = false;
 	}
@@ -28088,7 +28088,7 @@ inline VV_LOCAL_SYMBOL v__token__Token v__scanner__Scanner_new_eof_token(v__scan
 		.kind = v__token__Kind_eof,
 		.lit = _SLIT(""),
 		.line_nr = s->line_nr + 1,
-		.col = 1,
+		.col = v__scanner__Scanner_current_column(s),
 		.pos = s->pos,
 		.len = 1,
 		.tidx = s->tidx,
@@ -28484,7 +28484,7 @@ v__token__Token v__scanner__Scanner_buffer_scan(v__scanner__Scanner* s) {
 		v__token__Token _t1290 = (*(v__token__Token*)/*ee elem_typ */array_get(s->all_tokens, cidx));
 		return _t1290;
 	}
-	v__token__Token _t1291 = v__scanner__Scanner_new_token(s, v__token__Kind_eof, _SLIT(""), 1);
+	v__token__Token _t1291 = v__scanner__Scanner_new_eof_token(s);
 	return _t1291;
 }
 
